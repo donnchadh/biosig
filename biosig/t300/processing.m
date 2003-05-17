a226be94aa1e2aa2bd75a45394568e47ac0fbee1
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2003-05-17 16:29:47 +0000

    compatibility to Octave implemented

--- biosig/t300/processing.m
+++ biosig/t300/processing.m
@@ -14,7 +14,7 @@ function [Y,Z] = processing(MODE,X,Z)
 %    MODE is a struct and determines the signal processing method
 %
 %    MODE =
-%	{'ECG_envelope',Fs}       [1]
+%	{'ECG_envelope',Fs}       used for QRS-detection [1]
 %    	'xyz'		-none-	
 %
 %
@@ -25,8 +25,8 @@ function [Y,Z] = processing(MODE,X,Z)
 %
 
 
-%	$Revision: 1.1 $
-%	$Id: processing.m,v 1.1 2003-05-16 13:30:15 schloegl Exp $
+%	$Revision: 1.2 $
+%	$Id: processing.m,v 1.2 2003-05-17 16:29:47 schloegl Exp $
 %	Copyright (C) 2000-2003 by Alois Schloegl <a.schloegl@ieee.org>	
 
 % This library is free software; you can redistribute it and/or
@@ -54,7 +54,7 @@ else
 end;
 
 
-if 0
+if 0,
         
 elseif strcmp(D{1},'ECG_envelope')
         if isempty(Z),
@@ -63,32 +63,30 @@ elseif strcmp(D{1},'ECG_envelope')
                         return;
                 end;
                 
-                Fs = D{2};%.SampleRate;
-                
-                
-                tmp= Fs/200; 
+                Fs = D{2};   %.SampleRate;
+                                
+                tmp = Fs/200; 
                 
                 T = .005; % s
                 K = floor(T*Fs+1);
                 PAR.HI = zeros(1,4*(K+1)-1);
-                PAR.HI(1:2:end) = 2/pi*1./[-2*K-1:2:2*K+1];
+                PAR.HI(1:2:length(PAR.HI)) = 2/pi*1./[-2*K-1:2:2*K+1];
                 PAR.HI(2*K+2) = -j;
                 PAR.HI(:) = -j*PAR.HI(:);
-                PAR.trgL = ceil(0.04*Fs);              % 40ms
+                PAR.trgL = ceil(0.04*Fs);    % 40ms
 
                 Z.B2 = [1:PAR.trgL+1 PAR.trgL:-1:1];
                 Z.A2 = sum(abs(Z.B2));
                 
                 Z.B1 = conv(PAR.HI,[ones(1,T*Fs),-ones(1,T*Fs)]*1000/Fs);       
-                Z.A1 = 1; %sum(abs(MAT.ExFilt.B{26}));
+                Z.A1 = 1;
                 
-                Z.Z1 = [];
-                Z.Z2 = [];
+	        [tmp, Z.Z1] = filter(Z.B1,Z.A1,X);
+        	[Y,   Z.Z2] = filter(Z.B2,Z.A2,abs(tmp));
+        else
+	        [tmp, Z.Z1] = filter(Z.B1,Z.A1,X,Z.Z1);
+        	[Y,   Z.Z2] = filter(Z.B2,Z.A2,abs(tmp),Z.Z2);
         end;
-        
-        [tmp, Z.Z1] = filter(Z.B1,Z.A1,X,Z.Z1);
-        [Y,   Z.Z2] = filter(Z.B2,Z.A2,abs(tmp),Z.Z2);
-        
 else 
         
 end;
