1cf1b0b3a797f59c8f5aa83cfa8bd5c12c384c42
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2003-06-17 09:15:59 +0000

    estimates inital values for AAR estimation

---
+++ biosig/t300/getar0.m
@@ -0,0 +1,32 @@
+function [a0,A0] = getar0(S,P,NTR,NN);
+% GETAR0 calculates average AR parameters for initialization of 
+% AAR estimation
+% [a0,A0] = getar0(S,P,NTR,NN);
+%    S  signal 
+%    P  list of model orders
+%    NTR  number of realizations
+%    NN   length each segment
+%    a0 average AR-parameters	
+%    A0 covariance 
+%
+
+%	$Revision: 1.1 $
+%	$Id: getar0.m,v 1.1 2003-06-17 09:15:59 schloegl Exp $
+%	Copyright (c) 1996-2003 by Alois Schloegl
+%	e-mail: a.schloegl@ieee.org	
+
+
+S  = S(:);
+tmp= floor(rand(1,NTR)*(length(S)-NN));
+z0 = zeros(NTR,NN);
+for k = 1:length(tmp),
+        z0(k,:) = S(tmp(k) + (1:NN))';
+end;
+[MX,pe] = durlev(acovf(z0,max(P)));
+
+for k=1:length(P),
+        a = MX(:,P(k)*(P(k)-1)/2+(1:P(k)));
+        a0{k} = mean(a);
+        A0{k} = covm(a,'D');
+end;	
+
