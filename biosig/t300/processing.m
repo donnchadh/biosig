43f57ade3362acc6a238b6afed5f44711292d63a
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2003-07-23 06:53:04 +0000

    add QRS-detection

--- biosig/t300/processing.m
+++ biosig/t300/processing.m
@@ -15,6 +15,7 @@ function [Y,Z] = processing(MODE,X,Z)
 %
 %    MODE =
 %	{'ECG_envelope',Fs}       used for QRS-detection [1]
+%	{'ECG_envelope',Fs,Threshold}  used for QRS-detection [1]
 %    	'xyz'		-none-	
 %
 %
@@ -25,8 +26,8 @@ function [Y,Z] = processing(MODE,X,Z)
 %
 
 
-%	$Revision: 1.2 $
-%	$Id: processing.m,v 1.2 2003-05-17 16:29:47 schloegl Exp $
+%	$Revision: 1.3 $
+%	$Id: processing.m,v 1.3 2003-07-23 06:53:04 schloegl Exp $
 %	Copyright (C) 2000-2003 by Alois Schloegl <a.schloegl@ieee.org>	
 
 % This library is free software; you can redistribute it and/or
@@ -56,17 +57,31 @@ end;
 
 if 0,
         
+elseif strcmp(D{1},'AAR')
+        if length(D)<2, D{2}=[2,3]; end;
+        if length(D)<3, D{3}=[6]; end;
+        if length(D)<4, D{4}=2^(-55/8); end;
+        if length(D)<5, D{5}=eye(D{3}); end;
+        if length(D)<6, D{6}=zeros(1,D{3}); end;
+        
+        [Y,e,REV] = aar(X, D{2}, D{3}, D{4}, D{5}, D{6});
+        
 elseif strcmp(D{1},'ECG_envelope')
-        if isempty(Z),
+        if isempty(Z),	% Initialization
                 if length(D)<2,
                         fprintf(2,'ERROR: SampleRate not defined.\n');        
                         return;
+                else 
+                        Fs = D{2};   	%.SampleRate;
+                        if length(D)>2,
+                                T.TH = D{3};	% Threshold
+                                Z.datatype='QRS_events'    
+                                Z.QRS_event = [];
+                        else
+                                Z.datatype='ECG_envelope'    
+                                Z.ECGenvelope = [];
+                        end;
                 end;
-                
-                Fs = D{2};   %.SampleRate;
-                                
-                tmp = Fs/200; 
-                
                 T = .005; % s
                 K = floor(T*Fs+1);
                 PAR.HI = zeros(1,4*(K+1)-1);
@@ -74,19 +89,70 @@ elseif strcmp(D{1},'ECG_envelope')
                 PAR.HI(2*K+2) = -j;
                 PAR.HI(:) = -j*PAR.HI(:);
                 PAR.trgL = ceil(0.04*Fs);    % 40ms
-
+                
                 Z.B2 = [1:PAR.trgL+1 PAR.trgL:-1:1];
                 Z.A2 = sum(abs(Z.B2));
                 
                 Z.B1 = conv(PAR.HI,[ones(1,T*Fs),-ones(1,T*Fs)]*1000/Fs);       
                 Z.A1 = 1;
                 
-	        [tmp, Z.Z1] = filter(Z.B1,Z.A1,X);
-        	[Y,   Z.Z2] = filter(Z.B2,Z.A2,abs(tmp));
+                [tmp, Z.Z1] = filter(Z.B1,Z.A1,X);
+                [Y,   Z.Z2] = filter(Z.B2,Z.A2,abs(tmp));
+                
+                Z.tix = 0; 
+                Z.RESULT =[];
         else
-	        [tmp, Z.Z1] = filter(Z.B1,Z.A1,X,Z.Z1);
-        	[Y,   Z.Z2] = filter(Z.B2,Z.A2,abs(tmp),Z.Z2);
+                [tmp, Z.Z1] = filter(Z.B1,Z.A1,X,Z.Z1);
+                [Y,   Z.Z2] = filter(Z.B2,Z.A2,abs(tmp),Z.Z2);
+                if isfield(Z,'TH'),
+                        Y = gettrigger(Y,Z.TH);
+                        Z.QRS_event = [Z.QRS_event; (Y + Z.tix)/Fs];
+                        Z.tix    = Z.tix + size(Y,1);
+                else
+                        Z.ECGenvelope = [Z.ECGenvelope; Y];
+                end;
         end;
+        
+elseif strcmp(D{1},'CORRCOEF')
+        Y.datatype='CORRCOEF';
+        DIM  = size(X,2);
+        % SUM, N and SSQ 
+        %[Z.SUM,Z.N,Z.M2] = sumskipnan(X',2);
+        [Y.SUM,Y.N] = sumskipnan(X',2);
+        
+        % Correlation, using all possible values 
+        [Y.CC,Y.nN] = covm(X,'M');
+        
+        % Correlation of NaN's
+        [Y.nanCC,z.nan.nN] = covm(X,isnan(X),'M');
+        
+        % all samples with any NaN rejected 
+        tmp  = X(~any(isnan(X),2),:);
+        N0 = size(tmp,1);
+        Y.C0 = [ones(N0,1),tmp]' * tmp;
+        Y.N0 = N0;
+        
+        if isempty(Z),	% Initialization
+                Z = Y;         
+        else
+                % SUM, N and SSQ 
+                Z.SUM = Z.SUM + Y.SUM;
+                Z.N   = Z.N   + Y.N;
+                %Z.M2  = Z.M2  + z.M2;
+                
+                % Correlation, using all possible values 
+                Z.CC = Z.CC + Y.CC;
+                Z.nN = Z.nN + Y.nN;
+                
+                % Correlation of NaN's
+                Z.nanCC = Z.nanCC + Y.nanCC;
+                %Z.nannN = Z.nannN + Y.nannN;
+                
+                % all samples with any NaN rejected 
+                Z.C0 = Z.C0 + Y.C0;
+                Z.N0 = Z.N0 + Y.N0;
+        end;        
+        
 else 
         
 end;
