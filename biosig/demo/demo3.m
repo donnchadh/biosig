2ff42fcf8218426453160cc083a28b5c91bab4d3
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2003-10-06 08:15:57 +0000

    replace EEGfun by Sfun

--- biosig/demo/demo3.m
+++ biosig/demo/demo3.m
@@ -9,11 +9,11 @@
 %   LNX86	5.3.0.10183 (R11)	OK
 %   LNX86	6.5.0.180913a (R13)	OK
 % 	     OCTAVE
-%   i686-pc-cygwin	2.1.42		failed ## fwrite extends 0x0a to 0x0d 0x0a
+%   i686-pc-cygwin	2.1.42		OK
 %   i586-pc-linux-gnu   2.1.40		OK
 
-%	$Revision: 1.3 $
-%	$Id: demo3.m,v 1.3 2003-07-19 13:49:55 schloegl Exp $
+%	$Revision: 1.4 $
+%	$Id: demo3.m,v 1.4 2003-10-06 08:15:57 schloegl Exp $
 %	Copyright (C) 2000-2003 by Alois Schloegl <a.schloegl@ieee.org>	
 
 % This library is free software; you can redistribute it and/or
@@ -32,7 +32,7 @@
 % Boston, MA  02111-1307, USA.
 
 x = randn(10000,5)+(1:1e4)'*ones(1,5); % test data
- x = (1:1e4)'*ones(1,5); % test data
+x = (1:1e4)'*ones(1,5); % test data
 clear GDF;
 
 VER   = version;
@@ -85,14 +85,14 @@ GDF.DigMin  = [0;0;0;0;0];
 % define physical dimension
 GDF.PhysDim = ['uV ';'   ';'   ';'   ';'   '];
 
-GDF = eegopen(GDF,'w');
+GDF = sopen(GDF,'wb');
 %GDF.SIE.RAW = 0; % [default] channel data mode, one column is one channel 
 %GDF.SIE.RAW = 1; % switch to raw data mode, i.e. one column for one EDF-record
-GDF = eegwrite(GDF,x);
+GDF = swrite(GDF,x);
 
-GDF = eegclose(GDF);
+GDF = sclose(GDF);
 
-[s0,GDF0] = loadeeg(GDF.FileName);	% test file 
+[s0,GDF0] = sload(GDF.FileName);	% test file 
 
 plot(s0-x)
 
