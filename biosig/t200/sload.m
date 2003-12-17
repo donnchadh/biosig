4421c375e71777f13f3aecf9c39d7eabf5547240
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2003-12-17 19:36:39 +0000

    alpha-trace format: support of Event information included

--- biosig/t200/sload.m
+++ biosig/t200/sload.m
@@ -15,8 +15,8 @@ function [signal,H] = sload(FILENAME,CHAN,TYPE)
 % see also: SOPEN, SREAD, SCLOSE
 %
 
-%	$Revision: 1.4 $
-%	$Id: sload.m,v 1.4 2003-12-11 16:59:52 schloegl Exp $
+%	$Revision: 1.5 $
+%	$Id: sload.m,v 1.5 2003-12-17 19:36:39 schloegl Exp $
 %	Copyright (C) 1997-2003 by Alois Schloegl 
 %	a.schloegl@ieee.org	
 
@@ -59,7 +59,7 @@ H = sopen(H,'rb',CHAN);
 
 if H.FILE.FID>0,
         [signal,H] = sread(H);
-	H = sclose(H);
+        H = sclose(H);
  
 elseif strcmp(H.TYPE,'alpha'),
         H.VERSION = H.Version;
@@ -67,35 +67,65 @@ elseif strcmp(H.TYPE,'alpha'),
         H.SampleRate = H.SampleFreq;
         H.SPR = H.SampleCount;
 
-        if H.VERSION~=407.1;
+        if ~any(H.VERSION==[407.1,409.5]);
                 fprintf(2,'Warning SLOAD: Format ALPHA Version %6.2f not tested yet.\n',H.VERSION);
         end;
 
         fid = fopen(fullfile(p,'rawdata'),'rb');
-	H.VERSION2  = fread(fid,1,'int16');
-        H.NS   = fread(fid,1,'int16');
-        H.bits = fread(fid,1,'int16');
-	H.AS.bpb = H.NS*H.bits/8;
-
-        if H.BitsPerValue==12,
-                s = fread(fid,[3,inf],'uint8');
-                s(1,:) = bitshift(s(1,:),4)+ bitshift(s(2,:),-4); 	
-                s(3,:) = s(3,:)+ bitshift(bitand(s(2,:),15),8); 	
-                s = reshape(s([1,3],:),1,2*size(s,2));
-                signal = reshape(s(1:H.NS*H.SPR),H.NS,H.SPR)';
-                signal = signal-(signal>=2^11)*2^12;
-        elseif H.BitsPerValue==16,
-                s = fread(fid,[H.NS,inf],'int16');
-                signal = reshape(s(1:H.NS*H.SPR),H.NS,H.SPR)';
-        elseif H.BitsPerValue==32,
-                s = fread(fid,[H.NS,inf],'int32');
-                signal = reshape(s(1:H.NS*H.SPR),H.NS,H.SPR)';
+        if fid > 0,
+                H.VERSION2  = fread(fid,1,'int16');
+                H.NS   = fread(fid,1,'int16');
+                H.bits = fread(fid,1,'int16');
+                H.AS.bpb = H.NS*H.bits/8;
+                
+                if H.BitsPerValue==12,
+                        s = fread(fid,[3,inf],'uint8');
+                        s(1,:) = bitshift(s(1,:),4)+ bitshift(s(2,:),-4); 	
+                        s(3,:) = s(3,:)+ bitshift(bitand(s(2,:),15),8); 	
+                        s = reshape(s([1,3],:),1,2*size(s,2));
+                        signal = reshape(s(1:H.NS*H.SPR),H.NS,H.SPR)';
+                        signal = signal-(signal>=2^11)*2^12;
+                elseif H.BitsPerValue==16,
+                        s = fread(fid,[H.NS,inf],'int16');
+                        signal = reshape(s(1:H.NS*H.SPR),H.NS,H.SPR)';
+                elseif H.BitsPerValue==32,
+                        s = fread(fid,[H.NS,inf],'int32');
+                        signal = reshape(s(1:H.NS*H.SPR),H.NS,H.SPR)';
+                end;        
+                fclose(fid);
+                if any(CHAN),
+                        signal = signal(:,CHAN);
+                end;
         end;        
-        fclose(fid);
-        if any(CHAN),
-                signal = signal(:,CHAN);
-        end;
         
+        fid = fopen(fullfile(H.FILE.Path,'digin'),'rt');
+        if fid > 0,
+                [s] = fgetl(fid);
+                
+                k = 0; POS = []; TYP = []; IO = [];
+                while ~feof(fid),
+                        [s] = fgetl(fid);
+                        [timestamp,s] = strtok(s,'=');
+                        [type,io] = strtok(s,'=,');
+                        timestamp = str2num(timestamp);
+                        if ~isempty(timestamp),
+                                k = k + 1;
+                                POS(k) = timestamp;     
+                                TYP(k) = hex2dec(type);     
+                        else
+                                fprintf(2,'Warning SOPEN: alpha: invalid Event type\n');
+                        end;	                        
+                        if length(io)>1,
+                                IO(k) = io(2);
+                        end;
+                end;
+                fclose(fid);
+                H.EVENT.N   = k;
+                H.EVENT.POS = POS;
+                H.EVENT.TYP = TYP;
+                H.EVENT.IO  = IO;
+        end;
+
         
 elseif strcmp(H.TYPE,'DAQ')
 	fprintf(1,'Loading a matlab DAQ data file - this can take a while.\n');
