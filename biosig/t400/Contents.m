596455a09be774125c2d982494adbbeff205ca56
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2004-02-13 16:59:40 +0000

    Contents files added

---
+++ biosig/t400/Contents.m
@@ -0,0 +1,28 @@
+% BIOSIG/T400 contains classifiers.  
+%
+% LDBC	linear discriminant based classifier 
+% MDBC	mahalanobis distance based classifier 
+% LLBC	log-likelihood based classifier 
+% FINDCLASSIFIER1 generattes classifier for a BCI [2,3]
+%
+%
+% see also: NaN/COVM, DECOVM
+%
+%
+%
+% REFERENCES: 
+% [1] http://biosig.sf.net/
+% [2] Schlögl A., Neuper C. Pfurtscheller G.
+%	Estimating the mutual information of an EEG-based Brain-Computer-Interface.
+%	Biomedizinische Technik 47(1-2): 3-8, 2002
+%
+% [3] A. Schlögl, C. Keinrath, R. Scherer, G. Pfurtscheller,
+% 	Information transfer of an EEG-based Bran-computer interface.
+%	Proceedings of the 1st International IEEE EMBS Conference on Neural Engineering, 
+%	Capri, Italy, Mar 20-22, 2003.
+
+%	$Revision: 1.1 $
+%	$Id: Contents.m,v 1.1 2004-02-13 16:59:40 schloegl Exp $
+%	Copyright (c) 1997-2004 by Alois Schloegl
+%	a.schloegl@ieee.org	
+
---
+++ biosig/t500/Contents.m
@@ -0,0 +1,18 @@
+% BIOSIG/T500 display functions
+%
+% PLOTA         general display functions for BIOSIG
+% ELPOS         2-D electrode positions
+% ELPOS3        3-D electrode positions
+% 
+% see also: NaN/COVM, DECOVM
+%
+%
+% REFERENCES: 
+%
+
+%	$Revision: 1.1 $
+%	$Id: Contents.m,v 1.1 2004-02-13 16:59:39 schloegl Exp $
+%	Copyright (c) 1997-2004 by Alois Schloegl
+%	a.schloegl@ieee.org	
+%    	This is part of the BIOSIG-toolbox http://biosig.sf.net/
+
