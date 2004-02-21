d92ad905972d6f2399f1b6d400b59775b6e0fcd4
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2004-02-21 14:52:48 +0000

    simple signal viewer added

--- biosig/t500/Contents.m
+++ biosig/t500/Contents.m
@@ -1,17 +1,20 @@
 % BIOSIG/T500 display functions
 %
 % PLOTA         general display functions for BIOSIG
+% SVIEW		simple signal viewer
+%
 % ELPOS         2-D electrode positions
 % ELPOS3        3-D electrode positions
 % 
-% see also: NaN/COVM, DECOVM
+% 
+% see also: 
 %
 %
 % REFERENCES: 
 %
 
-%	$Revision: 1.1 $
-%	$Id: Contents.m,v 1.1 2004-02-13 16:59:39 schloegl Exp $
+%	$Revision: 1.2 $
+%	$Id: Contents.m,v 1.2 2004-02-21 14:52:47 schloegl Exp $
 %	Copyright (c) 1997-2004 by Alois Schloegl
 %	a.schloegl@ieee.org	
 %    	This is part of the BIOSIG-toolbox http://biosig.sf.net/
---
+++ biosig/t500/sview.m
@@ -0,0 +1,64 @@
+function sview(s,H),
+% SVIEW - a simple signal viewer 
+%    SVIEW(filename)
+%    SVIEW(HDR)
+%    SVIEW(S,HDR)
+%
+%
+% See also: SLOAD 
+
+%	$Revision: 1.1 $
+%	$Id: sview.m,v 1.1 2004-02-21 14:52:48 schloegl Exp $ 
+%	Copyright (c) 2004 by Alois Schlögl <a.schloegl@ieee.org>	
+%    	This is part of the BIOSIG-toolbox http://biosig.sf.net/
+
+% This program is free software; you can redistribute it and/or
+% modify it under the terms of the GNU General Public License
+% as published by the Free Software Foundation; either version 2
+% of the License, or (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+if ischar(s),
+        if exist(s)==2,
+                [s,H] = sload(s);
+        end;
+elseif isstruct(H)
+        [s,H] = sload(H);
+else 
+        return;
+end;
+
+t  = s(:); t(~isnan(t));
+dd = max(t)-min(t);
+
+plot((1:size(s,1))'/H.SampleRate,((s+(ones(size(s,1),1)*(1:size(s,2)))*dd/(-2)+4*dd)),'-');
+if H.EVENT.N > 0,
+        hold on;
+        if ~isfield(H.EVENT,'DUR');
+                plot(H.EVENT.POS/H.SampleRate, H.EVENT.CHN*dd, 'x');
+                
+        elseif 1,
+                plot([H.EVENT.POS,H.EVENT.POS+H.EVENT.DUR]'/H.SampleRate,[dd;dd]*H.EVENT.CHN','+-');    
+                
+        else
+                for k = 1:H.EVENT.N,
+                        plot(H.EVENT.POS(k)+[0,H.EVENT.DUR(k)],[0,0],'+-');    
+                end;
+        end;
+        hold off;
+end;
+
+title([H.FileName, ' generated with BIOSIG tools for Octave and Matlab(R)']);
+xlabel('time t[s]');
+ylabel(sprintf('Amplitude [%s]',H.PhysDim(1,:)));
+
+legend(H.Label);
+
