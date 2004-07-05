602a78191a9c06a8e1bb896bc9293fc324895b34
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2004-07-05 10:55:51 +0000

    use calibrated data

--- biosig/t250/regress_eog.m
+++ biosig/t250/regress_eog.m
@@ -1,11 +1,21 @@
-function R = regress_eog(D,ny,nx)
+function [R,S] = regress_eog(D,ny,nx)
 %  REGRESS_EOG yields the regression coefficients for 
 %  correcting EOG artifacts in EEG recordings.
 %  
-%  R = regress_eog(C,signal_channels,noise_channels)
+%  The correction of a single record is obtained like this:      
+%   [R,S2] = regress_eog(filename, el, ol)
+%   [R,S2] = regress_eog(S1, el, ol)
+%       
+%  Correction matrix is obtained through
+%   [R] = regress_eog(covm(S1,'E'), el, ol)
+%   S2 = S1 * R.ro;    % without offset correction
+%   S2 = [ones(size(S1,1),1),S1] * R.r1;    % with offset correction
 %  
-%  R    rereferencing matrix for correction artifacts with regression
-%  analysis
+%  S1   recorded data
+%  el   list of eeg channels: those channels will be corrected   
+%  ol   list of eog channels 
+%  R    rereferencing matrix for correction artifacts with regression analysis
+%  S2   corrected EEG-signal      
 %
 % see also: SAVE2BKR
 %
@@ -29,8 +39,8 @@ function R = regress_eog(D,ny,nx)
 % along with this program; if not, write to the Free Software
 % Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-%	$Revision: 1.2 $
-%	$Id: regress_eog.m,v 1.2 2004-06-29 18:17:52 schloegl Exp $
+%	$Revision: 1.3 $
+%	$Id: regress_eog.m,v 1.3 2004-07-05 10:55:51 schloegl Exp $
 %	(C) 1997-2004 by Alois Schloegl <a.schloegl@ieee.org>	
 %    	This is part of the BIOSIG-toolbox http://biosig.sf.net/
 
@@ -45,22 +55,19 @@ nx = nx(:);
 ny = ny(:);
 
 if ischar(D),
-        H=sopen(D);
-        H.FLAG.UCAL = 1;
-        [s,H] = sread(H);
-        H = sclose(H);
-        C = covm(s,'E');
+        [D,H] = sload(D);
+        C = covm(D,'E');
         
 elseif size(D,1)==size(D,2)
         C = D; 
-        NS = size(C,2)-1;
+        H.NS = size(C,2)-1;
 else
-        NS = size(D,2);
+        H.NS = size(D,2);
         C = covm(D,'E');
 end
 
-r0 = eye(NS);
-r1 = sparse(2:NS+1,1:NS,1);
+r0 = eye(H.NS);
+r1 = sparse(2:H.NS+1,1:H.NS,1);
 
 b0 = -inv(C([1;nx+1],[1;nx+1]))*C([1;nx+1],ny+1);
 r0(nx,ny) = b0(2:end,:);       % rearrange channel order
@@ -69,14 +76,16 @@ r1([1;1+nx],ny) = b0;       % rearrange channel order
 R.r0 = r0;
 R.r1 = r1;
 
-if ischar(D),
-        R = srewind(H);
-        R.Calib = [[1;zeros(H.NS,1)],r1] * H.Calib;
+if size(D,1)==size(D,2),
+        % R = R;         
         
-elseif size(D,1)==size(D,2)
-        R = R;         
-        
-else
-        R = [ones(size(D,1),1),D]*r1;
+else    
+        if ischar(D),
+                %R.Calib = [[1;zeros(H.NS,1)],r1] * H.Calib;
+                R.Calib = H.Calib * R.r0;
+        end;
+
+        % S = D * R.r0;   % without offset correction 
+        S = [ones(size(D,1),1),D] * R.r1; % with offset correction
 end
 
