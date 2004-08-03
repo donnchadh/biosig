d1221d03bfea226c78aa2aae5fa91faf327460f3
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2004-08-03 10:20:59 +0000

    resampling (partly) supported

--- biosig/t250/rs.m
+++ biosig/t250/rs.m
@@ -3,16 +3,33 @@ function [y1]=rs(y1,T,f2)
 % [y2] = rs(y1, f1, f2) resamples y1 with f1 to the target sampling rate f2 
 %
 % RS does not require overlap data. 
+%
+% see also: SOPEN, SREAD, SCLOSE, MAT2SEL, SAVE2TXT, SAVE2BKR
+%
+% Reference(s):
 
-%	Version 1.12
-%	21.Sep.2001
-%	Copyright (c) by 1999-2001 Alois Schloegl
+%	$Revision: 1.2 $
+%	$Id: rs.m,v 1.2 2004-08-03 10:20:59 schloegl Exp $
+%	Copyright (C) 1997-2004 by Alois Schloegl 
 %	a.schloegl@ieee.org	
+%    	This is part of the BIOSIG-toolbox http://biosig.sf.net/
 
-% .CHANGELOG
-% 21-09-2001 	f2>f1 implemented 
+% This library is free software; you can redistribute it and/or
+% modify it under the terms of the GNU Library General Public
+% License as published by the Free Software Foundation; either
+% Version 2 of the License, or (at your option) any later version.
+%
+% This library is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+% Library General Public License for more details.
+%
+% You should have received a copy of the GNU Library General Public
+% License along with this library; if not, write to the
+% Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+% Boston, MA  02111-1307, USA.
 
-if nargin==3
+if nargin==3,
         f1=T;
         if f1==f2
                 return;
@@ -28,7 +45,8 @@ if nargin==3
         else %f1<f2
 		y1=y1(ceil((1:size(y1,1)*f2/f1)/f2*f1),:);                
         end;
-elseif nargin==2
+        
+elseif nargin==2,
         [f1,f2]=size(T);
         if f1==f2,
                 return;
