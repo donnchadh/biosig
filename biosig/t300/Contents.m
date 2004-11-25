9b11e1352acbef3aaebbe5215e9ddf870656a33e
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2004-11-25 08:47:36 +0000

    George Townsends version of syntethic ECG

--- biosig/t300/Contents.m
+++ biosig/t300/Contents.m
@@ -1,5 +1,5 @@
 % BIOSIG/T300 contains signal processing functions for Matlab/Octave
-%     It requires also the TSA-toolbox [1].
+%     Several functions require the TSA-toolbox [1].
 %
 % Currently the following methods are supported:  
 %   Multivariate Autoregressive Analysis   
@@ -19,7 +19,11 @@
 %   EMG analysis
 %       Paynter
 %   ECG analysis
+%       processing 
+%       ecgbcorr
+%       qrscorr
 %       tvaar
+%	synthetic_ecg 	generates an ECG sequence 
 %   Others:
 %       Hjorth
 %       
@@ -31,8 +35,8 @@
 %
 %
 
-%	$Revision: 1.1 $
-%	$Id: Contents.m,v 1.1 2004-11-08 14:06:33 schloegl Exp $
+%	$Revision: 1.2 $
+%	$Id: Contents.m,v 1.2 2004-11-25 08:47:36 schloegl Exp $
 %	Copyright (C) 1997-2004 by Alois Schloegl <a.schloegl@ieee.org>	
 %	This is part of the BIOSIG project http://biosig.sf.net/
 
