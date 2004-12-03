b6e76141e69cd9710948b778f263928c8b7a3776
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2004-12-03 11:05:16 +0000

    calculates steepness of mutual information

---
+++ biosig/t490/criteria2005IIIb.m
@@ -0,0 +1,31 @@
+function [crit] = criteria2005IIIb(X)
+% criteria2005IIIb evalates the criterium according the 
+% BCI competition 2005, dataset IIIb. 
+%
+% see also: SUMSKIPNAN, PLOTA, BCI3EVAL, BCI4EVAL
+
+%    $Revision: 1.1 $
+%    $Id: criteria2005IIIb.m,v 1.1 2004-12-03 11:05:16 schloegl Exp $
+%    Copyright (C) 2003 by Alois Schloegl <a.schloegl@ieee.org>	
+%    This is part of the BIOSIG-toolbox http://biosig.sf.net/
+
+%    This program is free software; you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation; either version 2 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program; if not, write to the Free Software
+%    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+crit = NaN; 
+%if isfield(X,'datatype') & strcmp(X.datatype,'BCI_TSD7')
+if isfield(X,'T') & isfield(X,'I');
+        crit = max(X.I./(X.T-3).*(X.T>3.5));
+end;
