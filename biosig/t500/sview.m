133f2412ff5a2787f3e236a33ff0bb88b535c630
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2005-01-22 23:02:05 +0000

    added display of Events

--- biosig/t500/sview.m
+++ biosig/t500/sview.m
@@ -4,11 +4,10 @@ function [argout,s]=sview(s,H),
 %    SVIEW(HDR)
 %    SVIEW(S,HDR)
 %
-%
 % See also: SLOAD 
 
-%	$Revision: 1.7 $
-%	$Id: sview.m,v 1.7 2004-11-06 22:53:18 schloegl Exp $ 
+%	$Revision: 1.8 $
+%	$Id: sview.m,v 1.8 2005-01-22 23:02:05 schloegl Exp $ 
 %	Copyright (c) 2004 by Alois Schlögl <a.schloegl@ieee.org>	
 %    	This is part of the BIOSIG-toolbox http://biosig.sf.net/
 
@@ -35,6 +34,9 @@ if ischar(s),
         end;
 elseif isstruct(s)
         [s,H] = sload(s);
+
+elseif isnumeric(s) & (length(H.InChanSelect)==size(s,2))
+
 else 
         return;
 end;
@@ -66,7 +68,7 @@ end;
 %s = s*R.r0;
 
 if isfield(H,'Label'),
-        LEG = H.Label;
+        LEG = H.Label(H.InChanSelect,:);
 else
         LEG = '';
 end;
@@ -74,9 +76,56 @@ end;
 t = s(:); 
 %t(isnan(t))=median(t);
 dd = max(t)-min(t);
+%dd = max(std(s))*5;
+%s = zscore(s);
+%dd = 20;
 
-plot((1:size(s,1))'/H.SampleRate,((s+(ones(size(s,1),1)*(1:size(s,2)))*dd/(-2)+4*dd)),'-');
-if 0,H.EVENT.N > 0,
+H.AS.TIMECHAN = strmatch('Time',H.Label);
+if (length(H.FILE)==1) & any(H.AS.TIMECHAN) & any(H.AS.TIMECHAN==H.InChanSelect),
+	t = s(:,H.AS.TIMECHAN);
+	s(:,H.AS.TIMECHAN) = NaN;
+	X_Label = [H.Label(H.AS.TIMECHAN,:),' [',H.PhysDim(H.AS.TIMECHAN,:),']'];
+elseif isnan(H.SampleRate)
+	t = (1:size(s,1))';
+	X_Label = 'samples';
+else
+	t = (1:size(s,1))'/H.SampleRate;
+	X_Label = 'time [s]';
+end;
+
+if 0,
+elseif isfield(H,'SegLen'), 
+	EVENT.POS = H.SegLen(1:end-1)'+1;
+	EVENT.Desc = {H.FILE.Name}';
+	t = (1:size(s,1))';
+	X_Label = 'samples';
+elseif isfield(H.EVENT,'Desc'),
+	EVENT = H.EVENT;
+elseif ~isfield(H.EVENT,'Desc') & (length(H.EVENT.TYP)>0),
+	g = sload('eventcodes.txt');
+	ix = sparse(g.CodeIndex,1,1:length(g.CodeIndex));
+	EVENT.POS = H.EVENT.POS;
+	EVENT.Desc = {g.CodeDesc{ix(H.EVENT.TYP)}};
+end;
+
+plot(t,((s+(ones(size(s,1),1)*(1:size(s,2)))*dd/(-2)+4*dd)),'-');
+if isfield(EVENT,'Desc')
+	v = axis;
+	hold on
+	N = length(EVENT.POS);
+	plot([1;1]*t(EVENT.POS)',v(3:4),':k')
+	for k=1:length(EVENT.POS),
+		txt = EVENT.Desc{k}; 
+		if isempty(txt),txt='';end; 
+		txt(txt=='_')=' ';
+		h=text(t(EVENT.POS(k)),v(3:4)*[-.2;.8],txt);
+		set(h,'Rotation',90,'VerticalAlignment','top');
+	end;	
+	v(4) = v(3:4)*[-.7;1.3]; axis(v);
+	hold off;
+end;
+
+if 0, length(H.EVENT.POS) > 0,
         hold on;
         if 0, 
         elseif isfield(H.EVENT,'DUR') & isfield(H.EVENT,'CHN');
@@ -96,9 +145,13 @@ if 0,H.EVENT.N > 0,
         LEG = strvcat(LEG,'Events');
 end;
 
-tmp = H.FileName; tmp(find(tmp==92))='/'; tmp(find(tmp=='_'))=' ';
+if length(H.FILE)==1,
+    tmp = H.FileName; tmp(find(tmp==92))='/'; tmp(find(tmp=='_'))=' ';
+else
+    tmp = '';
+end;
 title([tmp, ' generated with BIOSIG tools for Octave and Matlab(R)']);
-xlabel('time t[s]');
+xlabel(X_Label);
 PhysDim = '';
 if ~isempty(H.PhysDim),
         PhysDim = deblank(H.PhysDim(1,:));
