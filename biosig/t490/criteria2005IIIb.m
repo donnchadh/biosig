e7e15d63192bd834a0c2b190a7a9efa4f037534e
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2005-06-04 22:06:50 +0000

    return time index

--- biosig/t490/criteria2005IIIb.m
+++ biosig/t490/criteria2005IIIb.m
@@ -1,4 +1,4 @@
-function [crit] = criteria2005IIIb(X)
+function [crit,t] = criteria2005IIIb(X)
 % criteria2005IIIb evalates the criterium according the 
 % BCI competition 2005, dataset IIIb. 
 %
@@ -8,9 +8,9 @@ function [crit] = criteria2005IIIb(X)
 % see also: SUMSKIPNAN, PLOTA, BCI3EVAL, BCI4EVAL
 
 
-%    $Revision: 1.2 $
-%    $Id: criteria2005IIIb.m,v 1.2 2004-12-09 16:53:31 schloegl Exp $
-%    Copyright (C) 2004 by Alois Schloegl <a.schloegl@ieee.org>	
+%    $Revision: 1.3 $
+%    $Id: criteria2005IIIb.m,v 1.3 2005-06-04 22:06:50 schloegl Exp $
+%    Copyright (C) 2004,2005 by Alois Schloegl <a.schloegl@ieee.org>	
 %    This is part of the BIOSIG-toolbox http://biosig.sf.net/
 
 %    This program is free software; you can redistribute it and/or modify
@@ -31,5 +31,6 @@ function [crit] = criteria2005IIIb(X)
 crit = NaN; 
 %if isfield(X,'datatype') & strcmp(X.datatype,'BCI_TSD7')
 if isfield(X,'T') & isfield(X,'I');
-        crit = max(X.I./(X.T-3).*(X.T>3.5));
+        [crit,t] = max(X.I./(X.T-3).*(X.T>3.5));
+        t = X.T(t);
 end;
