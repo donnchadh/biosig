9b052e24940237b6a03cb923b449a124ccd4f0ce
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2006-08-08 07:59:42 +0000

    groups are supported - enables more flexible jackknife ; classes are supported too

--- biosig/t300/tfmvar.m
+++ biosig/t300/tfmvar.m
@@ -1,10 +1,9 @@
-function [M,SE,R] = tfmvar(s,TRIG,T,MOP,f,Fs)
+function [R] = tfmvar(s,TRIG,T,MOP,f,Fs,cl)
 % TFMVAR Time-Frequency MVAR analysis
 %   time-frequency analysis of 
 %   multivariate stochastic processes. 
 %
-% [R] = tfmvar(s,TRIG,T,MOP,f,Fs)
-% [M,SE,R] = tfmvar(s,TRIG,T,MOP,f,Fs)
+% [R] = tfmvar(s,TRIG,T,MOP,f,Fs, [CL,group])
 %
 % INPUT: 
 %  s    signal data (one channel per column) 
@@ -14,6 +13,18 @@ function [M,SE,R] = tfmvar(s,TRIG,T,MOP,f,Fs)
 %  MOP  model order of the MVAR model   
 %  f    designated frequencies 
 %  Fs   sampling rate. 
+%  [CL,group]  is OPTIONAL
+%	CL 	are the labels for different classes, conditions, states. 
+%		CL must be a column vector having the same length than TRIG
+%       group 	is useful for controlling the resampling
+%		same numbers indicate that member belongs to the same group. 
+%		E.g. if data from several subjects are concatanated, and the 
+%		the trials of each subject have the same numbers, the standard error 
+%		of the group-statistic is estimated. 
+%		If group is empty [default], each trial gets a different number; 
+%		Accordingly, a trial-based leave-on-out-method (LOOM) is used, 
+%		for computing the standard error. 
+%
 %
 % OUTPUT: 
 %     	M and SE contain the mean 
@@ -34,6 +45,8 @@ function [M,SE,R] = tfmvar(s,TRIG,T,MOP,f,Fs)
 %   h		transfer function 		
 %   logS   	log(abs(S))
 %   logh   	log(abs(h))
+%   y1i         imaginary part of amplitude spectra 
+%   h1i         imaginary part of transfer function
 %   phaseS   	phase of S
 %   phaseh   	phase of h
 %   COH		coherence
@@ -84,10 +97,12 @@ function [M,SE,R] = tfmvar(s,TRIG,T,MOP,f,Fs)
 % [6] Kaminski M., Ding M., Truccolo W. A., Bressler S. L., Evaluating Causal Relations in Neural Systems: Granger Causality, Directed Transfer Function and Statistical Assessment of Significance, Biological Cybernetics 85, 2001
 % [7] Liang H., Ding M., Bressler S. L., On the Tracking of Dynamic Functional Relations in Monkey Cerebral Cortex, Neurocomputing, 2000
 % [8] Korzeniewska A., Manczak M., Kaminski M., Blinowska K. J., Kasicki S., Determination of Information Flow Direction Among Brain Structures By a Modified Directed Transfer Function (dDTF) Method, Journal of Neuroscience Methods 125, 2003
+% [9] A. Schl\"ogl, Comparison of Multivariate Autoregressive Estimators. Signal processing, Elsevier B.V. (in press). 
+%       available at http://dx.doi.org/doi:10.1016/j.sigpro.2005.11.007
 
-%	$Revision: 1.6 $
-%	$Id: tfmvar.m,v 1.6 2006-01-07 23:48:34 schloegl Exp $
-%	Copyright (C) 2004 by Alois Schloegl <a.schloegl@ieee.org>	
+%	$Revision: 1.7 $
+%	$Id: tfmvar.m,v 1.7 2006-08-08 07:59:42 schloegl Exp $
+%	Copyright (C) 2004,2005,2006 by Alois Schloegl <a.schloegl@ieee.org>	
 %    	This is part of the BIOSIG-toolbox http://biosig.sf.net/
 
 
@@ -111,11 +126,40 @@ FLAG.SEM = 1;
 R.datatype = 'TF-MVAR 2.0';	% C = PE(MOP+1)
 %R.datatype = 'TF-MVAR';	% SE = SE/N,  C = PE(MOP+1)'
 
-R.N = length(TRIG);
-R.T = (T(1,:)+T(2,:))/(2*Fs); 
+R.T = T; %(T(1,:)+T(2,:))/(2*Fs); 
+R.SampleRate = Fs; 
 R.F = f; 
 R.MOP = MOP; 
 R.nan_ratio = zeros(1,size(T,2));
+
+if nargin<7,
+	cl = ones(length(TRIG),1); 
+end;	
+tmp=cl;tmp(isnan(tmp))=0;
+if any(rem(tmp,1) & ~isnan(cl)),
+        fprintf(2,'Error %s: class information is not integer\n',mfilename);
+        return;
+end;
+if length(TRIG)~=length(cl);
+        fprintf(2,'Warning TFMVAR: number of Triggers do not match class information');
+end;
+if size(cl,1)~=length(TRIG);    
+        fprintf(2,'Warning TFMVAR: Classlabels must be a column vector');
+        if length(TRIG)==size(cl,2),
+                cl = cl';
+        end;
+end;
+
+TRIG = TRIG(:);
+TRIG(any(isnan(cl),2))=[];
+cl(any(isnan(cl),2))=[];
+if size(cl,2)>1,
+        cl2 = cl(:,2);          % 2nd column contains the group definition, ( Leave-One (Group) - Out ) 
+        cl  = cl(:,1); 
+else
+        cl2 = [1:length(cl)]';  % each trial is a group (important for cross-validation); Trial-based LOOM  
+end;
+CL = unique(cl);
 
 
 % univariate
@@ -147,6 +191,9 @@ C1    = zeros(sz);
 sz = [m,m,length(f),size(T,2)];
 R.M.S = zeros(sz);
 R.M.h = zeros(sz);
+R.M.iY = zeros(sz);
+R.M.ih = zeros(sz);
+R.M.dT = zeros(sz);
 R.M.phaseS = zeros(sz);
 R.M.phaseh = zeros(sz);
 R.M.logS   = zeros(sz);
@@ -168,28 +215,18 @@ R.M.AR = zeros(sz);
 
 R.SE  = R.M;
 
-sz    = [m,m,length(f),length(TRIG)];
-S     = zeros(sz);
-h     = zeros(sz);
-COH   = zeros(sz);
-coh   = zeros(sz);
-PDC   = zeros(sz);
-PDCF  = zeros(sz);
-DTF   = zeros(sz);
-pCOH  = zeros(sz);
-dDTF  = zeros(sz);
-ffDTF = zeros(sz);
-pCOH2 = zeros(sz);
-sz(3) = 1;
-C     = zeros(sz);
-DC    = zeros(sz);
-sz(2) = m*MOP;
-AR    = zeros(sz);
-
-tic,
+r=R; clear R; 
+for k0 = 1:length(CL); 
+	R{k0} = r;
+	ix0 = find(cl==CL(k0));	
+	trig = TRIG(ix0);
+       	CL2 = unique(cl2(ix0)); 
+	R{k0}.N = length(CL2); 
+     	
+tic;
 for k1 = 1:size(T,2),
-        [S0,sz0] = trigg(s,TRIG,T(1,k1),T(2,k1),1+MOP);
-        R.nan_ratio(k1) = mean(isnan(S0(:)));
+        [S0,sz0] = trigg(s,trig,T(1,k1),T(2,k1),1+MOP);
+        R{k0}.nan_ratio(k1) = mean(isnan(S0(:)));
         % univariate
         [AR1,RC1,PE1] = durlev(acovf(S0,MOP));
         
@@ -200,10 +237,10 @@ for k1 = 1:size(T,2),
         
         %[S(:,:,:,k2),  h(:,:,:,k2), PDC(:,:,:,k2), COH(:,:,:,k2), DTF(:,:,:,k2), DC(:,:,1,k2), pCOH(:,:,:,k2), dDTF(:,:,:,k2), ffDTF(:,:,:,k2), pCOH2(:,:,:,k2),coh(:,:,:,k2)] = mvfreqz(X.B,X.A,X.C,f,Fs);
         
-        R.M.S1(:,:,k1) = H1; 
-        R.M.logS1(:,:,k1) = log(abs(H1)); 
-        R.M.AR1(:,:,k1) = AR1; 
-        R.M.C1(:,:,k1)  = PE1;
+        R{k0}.M.S1(:,:,k1) = H1; 
+        R{k0}.M.logS1(:,:,k1) = log(abs(H1)); 
+        R{k0}.M.AR1(:,:,k1) = AR1; 
+        R{k0}.M.C1(:,:,k1)  = PE1;
         
         % multivariate 
         [A,RC,PE] = mvar(S0',MOP,5);
@@ -213,32 +250,55 @@ for k1 = 1:size(T,2),
         X.datatype = 'MVAR';
         
         [S, h, PDC, COH, DTF, DC, pCOH, dDTF, ffDTF, pCOH2, PDCF, coh] = mvfreqz(X.B,X.A,X.C,f,Fs);
-        
-        R.M.phaseS(:,:,:,k1) = angle(S);
-        R.M.phaseh(:,:,:,k1) = angle(h);
-        R.M.S(:,:,:,k1)      = S;
-        R.M.h(:,:,:,k1)      = h;
-        R.M.logS(:,:,:,k1)   = log(abs(S));
-        R.M.logh(:,:,:,k1)   = log(abs(h));
-        R.M.PDC(:,:,:,k1)    = PDC;
-        R.M.PDCF(:,:,:,k1)   = PDCF;
-        R.M.COH(:,:,:,k1)    = abs(COH);
-        R.M.coh(:,:,:,k1)    = abs(coh);
-        R.M.iCOH(:,:,:,k1)   = imag(COH);
-        R.M.icoh(:,:,:,k1)   = imag(coh);
-        R.M.DTF(:,:,:,k1)    = DTF;
-        R.M.pCOH(:,:,:,k1)   = pCOH;
-        R.M.dDTF(:,:,:,k1)   = dDTF;
-        R.M.ffDTF(:,:,:,k1)  = ffDTF;
-        R.M.pCOH2(:,:,:,k1)  = pCOH2;
-        R.M.AR(:,:,1,k1)     = A;
-        R.M.DC(:,:,1,k1)     = DC;
-        R.M.C(:,:,1,k1)      = X.C;
+
+	R{k0}.M.dT(:,:,:,k1)     = angle(S)./reshape(repmat(2*pi*f(:),[m*m,1]),[m,m,length(f)]);
+        R{k0}.M.phaseS(:,:,:,k1) = angle(S);
+        R{k0}.M.phaseh(:,:,:,k1) = angle(h);
+        R{k0}.M.S(:,:,:,k1)      = S;   % power spectra
+        R{k0}.M.h(:,:,:,k1)      = h;   % complex amplitude spectra (auto- and cross-spectra)
+        R{k0}.M.ih(:,:,:,k1)     = imag(h);   % complex amplitude spectra (auto- and cross-spectra)
+        R{k0}.M.iY(:,:,:,k1)     = h.*repmat(sqrtm(X.C/(2*pi*Fs)),[1,1,length(f)]);   % complex amplitude spectra (auto- and cross-spectra)
+        R{k0}.M.logS(:,:,:,k1)   = log(abs(S));
+        R{k0}.M.logh(:,:,:,k1)   = log(abs(h));
+        R{k0}.M.PDC(:,:,:,k1)    = PDC;
+        R{k0}.M.PDCF(:,:,:,k1)   = PDCF;
+        R{k0}.M.COH(:,:,:,k1)    = abs(COH);
+        R{k0}.M.coh(:,:,:,k1)    = abs(coh);
+        R{k0}.M.iCOH(:,:,:,k1)   = imag(COH);
+        R{k0}.M.icoh(:,:,:,k1)   = imag(coh);
+        R{k0}.M.DTF(:,:,:,k1)    = DTF;
+        R{k0}.M.pCOH(:,:,:,k1)   = pCOH;
+        R{k0}.M.dDTF(:,:,:,k1)   = dDTF;
+        R{k0}.M.ffDTF(:,:,:,k1)  = ffDTF;
+        R{k0}.M.pCOH2(:,:,:,k1)  = pCOH2;
+        R{k0}.M.AR(:,:,1,k1)     = A;
+        R{k0}.M.DC(:,:,1,k1)     = DC;
+        R{k0}.M.C(:,:,1,k1)      = X.C;
         
         if FLAG.SEM,
-                for k2 = 1:length(TRIG),
-                        sel = [1:k2-1,k2+1:length(TRIG)];
-                        [S0,sz0] = trigg(s, TRIG(sel), T(1,k1), T(2,k1), 1+MOP);
+        
+		sz    = [m,m,length(f),length(CL2)];
+		S     = zeros(sz);
+		h     = zeros(sz);
+		COH   = zeros(sz);
+		coh   = zeros(sz);
+		PDC   = zeros(sz);
+		PDCF  = zeros(sz);
+		DTF   = zeros(sz);
+		pCOH  = zeros(sz);
+		dDTF  = zeros(sz);
+		ffDTF = zeros(sz);
+		pCOH2 = zeros(sz);		
+		sz(3) = 1;
+		C     = zeros(sz);
+		DC    = zeros(sz);
+		sz(2) = m*MOP;
+		AR    = zeros(sz);
+
+
+                for k2 = 1:length(CL2),
+                        sel = (cl2(ix0)~=CL2(k2)); 
+                        [S0,sz0] = trigg(s, trig(sel), T(1,k1), T(2,k1), 1+MOP);
                         %fprintf(2,'\nExtract epochs done\nNumber of Trials: %i : %i\n',length(t0),length(t1));
                         
                         % univariate
@@ -263,64 +323,69 @@ for k1 = 1:size(T,2),
                         X.datatype = 'MVAR';
                         
                         [S(:,:,:,k2),  h(:,:,:,k2), PDC(:,:,:,k2), COH(:,:,:,k2), DTF(:,:,:,k2), DC(:,:,1,k2), pCOH(:,:,:,k2), dDTF(:,:,:,k2), ffDTF(:,:,:,k2), pCOH2(:,:,:,k2),PDCF(:,:,:,k2), coh(:,:,:,k2)] = mvfreqz(X.B,X.A,X.C,f,Fs);
-                        
-                        AR(:,:,1,k2) = A; 
+
+                        Y(:,:,:,k1)  = h(:,:,:,k2).*repmat(sqrtm(X.C/(2*pi*Fs)),[1,1,length(f)]);   % complex amplitude spectra (auto- and cross-spectra)
+
+                        AR(:,:,1,k2) = A;
                         C(:,:,1,k2)  = X.C;
                         
                         fprintf(2,'%.0f\t%.0f\t%.1f\n',[k1,k2,toc]);tic;
                 end;
                 
                 % univariate
-                [R.SE.S1(:,:,k1),   R.M.S1(:,:,k1)  ] = sem(abs(S1),3);
-                [R.SE.logS1(:,:,k1),R.M.logS1(:,:,k1)] = sem(log(abs(S1)),3);
-                [R.SE.AR1(:,:,k1),  R.M.AR1(:,:,k1) ] = sem(A1,3);
-                [R.SE.C1(:,:,k1),   R.M.C1(:,:,k1)  ] = sem(C1,3);
-                
-                
+                [R{k0}.SE.S1(:,:,k1),   R{k0}.M.S1(:,:,k1)  ] = sem(abs(S1),3);
+                [R{k0}.SE.logS1(:,:,k1),R{k0}.M.logS1(:,:,k1)] = sem(log(abs(S1)),3);
+                [R{k0}.SE.AR1(:,:,k1),  R{k0}.M.AR1(:,:,k1) ] = sem(A1,3);
+                [R{k0}.SE.C1(:,:,k1),   R{k0}.M.C1(:,:,k1)  ] = sem(C1,3);
+
                 % multivariate 
-                [R.SE.phaseS(:,:,:,k1), R.M.phaseS(:,:,:,k1)] = sem(angle(S),4);
-                [R.SE.phaseh(:,:,:,k1), R.M.phaseh(:,:,:,k1)] = sem(angle(h),4);
-                [R.SE.S(:,:,:,k1),      R.M.S(:,:,:,k1)     ] = sem(S,4);
-                [R.SE.h(:,:,:,k1),      R.M.h(:,:,:,k1)     ] = sem(h,4);
-                [R.SE.logS(:,:,:,k1),   R.M.logS(:,:,:,k1)  ] = sem(log(abs(S)),4);
-                [R.SE.logh(:,:,:,k1),   R.M.logh(:,:,:,k1)  ] = sem(log(abs(h)),4);
-                [R.SE.PDC(:,:,:,k1),    R.M.PDC(:,:,:,k1)   ] = sem(PDC,4);
-                [R.SE.PDCF(:,:,:,k1),   R.M.PDCF(:,:,:,k1)  ] = sem(PDCF,4);
-                [R.SE.COH(:,:,:,k1),    R.M.COH(:,:,:,k1)   ] = sem(abs(COH),4);
-                [R.SE.coh(:,:,:,k1),    R.M.coh(:,:,:,k1)   ] = sem(abs(coh),4);
-                [R.SE.iCOH(:,:,:,k1),   R.M.iCOH(:,:,:,k1)  ] = sem(imag(COH),4);
-                [R.SE.icoh(:,:,:,k1),   R.M.icoh(:,:,:,k1)  ] = sem(imag(coh),4);
-                [R.SE.DTF(:,:,:,k1),    R.M.DTF(:,:,:,k1)   ] = sem(DTF,4);
-                [R.SE.pCOH(:,:,:,k1),   R.M.pCOH(:,:,:,k1)  ] = sem(pCOH,4);
-                [R.SE.dDTF(:,:,:,k1),   R.M.dDTF(:,:,:,k1)  ] = sem(dDTF,4);
-                [R.SE.ffDTF(:,:,:,k1),  R.M.ffDTF(:,:,:,k1) ] = sem(ffDTF,4);
-                [R.SE.pCOH2(:,:,:,k1),  R.M.pCOH2(:,:,:,k1) ] = sem(pCOH2,4);
-                [R.SE.AR(:,:,1,k1),     R.M.AR(:,:,1,k1)    ] = sem(AR,4);
-                [R.SE.DC(:,:,1,k1),     R.M.DC(:,:,1,k1)    ] = sem(DC,4);
-                [R.SE.C(:,:,1,k1),      R.M.C(:,:,1,k1)     ] = sem(C,4);
+                [R{k0}.SE.dT(:,:,:,k1),     R{k0}.M.dT(:,:,:,k1)]     = sem(angle(S)./permute(reshape(repmat(2*pi*f(:)',[m*m*length(CL2),1]),[m,m,length(CL2),length(f)]),[1,2,4,3]),4);
+                [R{k0}.SE.phaseS(:,:,:,k1), R{k0}.M.phaseS(:,:,:,k1)] = sem(angle(S),4);
+                [R{k0}.SE.phaseh(:,:,:,k1), R{k0}.M.phaseh(:,:,:,k1)] = sem(angle(h),4);
+                [R{k0}.SE.iY(:,:,:,k1),     R{k0}.M.iY(:,:,:,k1)]     = sem(imag(Y),4);
+                [R{k0}.SE.ih(:,:,:,k1),     R{k0}.M.ih(:,:,:,k1)]     = sem(imag(h),4);
+                [R{k0}.SE.dT1(:,:,:,k1),    R{k0}.M.dT1(:,:,:,k1)]    = sem(angle(coh)./permute(reshape(repmat(2*pi*f(:)',[m*m*length(CL2),1]),[m,m,length(CL2),length(f)]),[1,2,4,3]),4);
+                [R{k0}.SE.phaseS1(:,:,:,k1),R{k0}.M.phaseS1(:,:,:,k1)]= sem(angle(coh),4);
+                [R{k0}.SE.phaseS2(:,:,:,k1),R{k0}.M.phaseS2(:,:,:,k1)]= sem(angle(COH),4);  % for testing: should be same than phaseS
+                [R{k0}.SE.S(:,:,:,k1),      R{k0}.M.S(:,:,:,k1)     ] = sem(S,4);
+                [R{k0}.SE.h(:,:,:,k1),      R{k0}.M.h(:,:,:,k1)     ] = sem(h,4);
+                [R{k0}.SE.logS(:,:,:,k1),   R{k0}.M.logS(:,:,:,k1)  ] = sem(log(abs(S)),4);
+                [R{k0}.SE.logh(:,:,:,k1),   R{k0}.M.logh(:,:,:,k1)  ] = sem(log(abs(h)),4);
+                [R{k0}.SE.PDC(:,:,:,k1),    R{k0}.M.PDC(:,:,:,k1)   ] = sem(PDC,4);
+                [R{k0}.SE.PDCF(:,:,:,k1),   R{k0}.M.PDCF(:,:,:,k1)  ] = sem(PDCF,4);
+                [R{k0}.SE.COH(:,:,:,k1),    R{k0}.M.COH(:,:,:,k1)   ] = sem(abs(COH),4);
+                [R{k0}.SE.coh(:,:,:,k1),    R{k0}.M.coh(:,:,:,k1)   ] = sem(abs(coh),4);
+                [R{k0}.SE.iCOH(:,:,:,k1),   R{k0}.M.iCOH(:,:,:,k1)  ] = sem(imag(COH),4);
+                [R{k0}.SE.icoh(:,:,:,k1),   R{k0}.M.icoh(:,:,:,k1)  ] = sem(imag(coh),4);
+                [R{k0}.SE.DTF(:,:,:,k1),    R{k0}.M.DTF(:,:,:,k1)   ] = sem(DTF,4);
+                [R{k0}.SE.pCOH(:,:,:,k1),   R{k0}.M.pCOH(:,:,:,k1)  ] = sem(pCOH,4);
+                [R{k0}.SE.dDTF(:,:,:,k1),   R{k0}.M.dDTF(:,:,:,k1)  ] = sem(dDTF,4);
+                [R{k0}.SE.ffDTF(:,:,:,k1),  R{k0}.M.ffDTF(:,:,:,k1) ] = sem(ffDTF,4);
+                [R{k0}.SE.pCOH2(:,:,:,k1),  R{k0}.M.pCOH2(:,:,:,k1) ] = sem(pCOH2,4);
+                [R{k0}.SE.AR(:,:,1,k1),     R{k0}.M.AR(:,:,1,k1)    ] = sem(AR,4);
+                [R{k0}.SE.DC(:,:,1,k1),     R{k0}.M.DC(:,:,1,k1)    ] = sem(DC,4);
+                [R{k0}.SE.C(:,:,1,k1),      R{k0}.M.C(:,:,1,k1)     ] = sem(C,4);
                 
         end; 
         %% In order to obtain the standard error of the mean, 
         %% this SE must be multiplied by N-1, with N = length(TRIG)
-end;
+        
+end; 	%for k1
+
 
 %%%%% scaling of standard error 
-GF = fieldnames(R.SE);
+GF = fieldnames(R{k0}.SE);
 for k2=1:length(GF),
-      	R.SE = setfield(R.SE,GF{k2},getfield(R.SE,GF{k2})*(R.N-1));
+      	R{k0}.SE = setfield(R{k0}.SE,GF{k2},getfield(R{k0}.SE,GF{k2})*(R{k0}.N-1));
 end;
 
-if any(R.nan_ratio),
-        fprintf(2,'Warning TFMVAR: up to %5.2f%% are missing values (NaN).\n',max(R.nan_ratio)*100);
+if any(R{k0}.nan_ratio),
+        fprintf(2,'Warning TFMVAR: up to %5.2f%% are missing values (NaN).\n',max(R{k0}.nan_ratio)*100);
         fprintf(2,'This may cause underestimating the standard error.\n');
 end;	
 
-if nargout > 1, 
-        M = R.M;
-        R = rmfield(R,'M');
-        SE= R.SE; 
-        R = rmfield(R,'SE');
-else 
-        M = R;
-end;
+
+end; 	%for k0
+
+
 
