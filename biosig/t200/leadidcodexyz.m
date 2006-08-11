f84268b6d302c46e3d9ee0a8e7f1481009ce9a93
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2006-08-11 23:02:35 +0000

    support of SCPECG - LeadIdCode included

--- biosig/t200/leadidcodexyz.m
+++ biosig/t200/leadidcodexyz.m
@@ -27,7 +27,7 @@ function [HDR] = leadidcodexyz(arg1)
 % along with this program; if not, write to the Free Software
 % Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-%	$Id: leadidcodexyz.m,v 1.4 2006-08-11 16:35:21 schloegl Exp $
+%	$Id: leadidcodexyz.m,v 1.5 2006-08-11 23:02:35 schloegl Exp $
 %	Copyright (C) 2006 by Alois Schloegl <a.schloegl@ieee.org>	
 %    	This is part of the BIOSIG-toolbox http://biosig.sf.net/
 
@@ -45,75 +45,134 @@ if ~BIOSIG_GLOBAL.ISLOADED_XYZ;
 	
         BIOSIG_GLOBAL.ISLOADED_XYZ = 0 ;
 
+        N = 0;
+        fid = fopen(fullfile(p,'doc','leadidtable_scpecg.txt'),'r');
+        s = char(fread(fid,[1,inf],'char')); 
+        fclose(fid);
+        
+        Code = repmat(NaN, 200, 1); Phi = Code; Theta = Code;
+        while ~isempty(s),
+        	[t,s] = strtok(s,[10,13]);
+                if ~length(t)
+                elseif ~strncmp(t,'#',1)    
+                	ix3 = strfind(t,'MDC_ECG_LEAD_');
+                        [t1,t2] = strtok(t(1:ix3-1),[9,32]);
+                        [t2,t3] = strtok(t2,[9,32]);
+                        id = str2double(t2);
+                        N  = N + 1;
+                        Labels{N,1}	  = t1;
+                        Code(N,1)         = id;
+                        Description{N,1}  = deblank(t3);
+                        MDC_ECG_LEAD{N,1} = t(ix3+13:end);
+                end;
+        end;
+        N1 = N;
+
+
         % load table 
         fid = fopen(fullfile(p,'doc','elecpos.txt'),'r');
         t = char(fread(fid,[1,inf],'char'));
         fclose(fid);
 
         % extract table information       
-        N = 0;
-        Code = repmat(NaN,100,1); Phi=Code; Theta=Code;
         while ~isempty(t)
                 [x,t] = strtok(t,[10,13]);
                 if isempty(x)
                 elseif strncmp(x,'#',1)
                 else
-                        N = N+1;
+                        N = N + 1;
                         [num,status,strarray] = str2double(x);
-                        Code(N) = num(1);
-                        Labels{N,1}=upper(strarray{2});
-                        Phi(N) = num(3);
-                        Theta(N) = num(4);
+                        Code(N,1)   = num(1);
+                        Labels{N,1} = upper(strarray{2});
+                        Phi(N,1)    = num(3);
+                        Theta(N,1)  = num(4);
                 end;
         end;
         Phi   = Phi(:)  *pi/180;
         Theta = Theta(:)*pi/180;
         
+        
         % loading is done only once. 
-        BIOSIG_GLOBAL.XYZ   = [sin(Theta).*cos(Phi), sin(Theta).*sin(Phi), cos(Theta)];
-        BIOSIG_GLOBAL.LeadIdCode = Code;
-        BIOSIG_GLOBAL.Label = Labels;
+        BIOSIG_GLOBAL.XYZ = [sin(Theta).*cos(Phi), sin(Theta).*sin(Phi), cos(Theta)];
+        BIOSIG_GLOBAL.LeadIdCode   = Code;
+        BIOSIG_GLOBAL.Label        = Labels;
+        BIOSIG_GLOBAL.Description  = Description;
+        BIOSIG_GLOBAL.MDC_ECG_LEAD = MDC_ECG_LEAD;
 
-        BIOSIG_GLOBAL.ISLOADED_XYZ = 1 ;
+
+        BIOSIG_GLOBAL.ISLOADED_XYZ = 1;
 end; 
 
 
 if nargin<1,
         HDR.LeadIdCode = BIOSIG_GLOBAL.LeadIdCode;
-        HDR.Label = BIOSIG_GLOBAL.Label;
-        HDR.ELEC.XYZ = BIOSIG_GLOBAL.XYZ; 
-        HDR.TYPE = 'ELPOS'; 
+        HDR.Label      = BIOSIG_GLOBAL.Label;
+        HDR.ELEC.XYZ   = BIOSIG_GLOBAL.XYZ; 
+        HDR.TYPE       = 'ELPOS'; 
         
 else    % electrode code and position
+
         if isstruct(arg1)
                 HDR = arg1; 
+        elseif isnumeric(arg1),
+                HDR.LeadIdCode = arg1; 
         else
                 HDR.Label = arg1; 
-        end; 
+        end;
+
         tmp.flag1 = isfield(HDR,'ELEC');
         if tmp.flag1,
                 tmp.flag1 = isfield(HDR.ELEC,'XYZ');
         end;
         tmp.flag2 = isfield(HDR,'LeadIdCode');
+        tmp.flag3 = isfield(HDR,'Label');
 
-        if (~tmp.flag1 | ~tmp.flag2),
-                HDR.Label = cellstr(HDR.Label); 
-                for k = 1:length(HDR.Label);
-                        ix = strmatch(upper(deblank(HDR.Label{k})),BIOSIG_GLOBAL.Label);
-                        if (length(ix)==1),
-                                LeadIdCode = BIOSIG_GLOBAL.LeadIdCode(ix);
-                                XYZ = BIOSIG_GLOBAL.XYZ(ix,:);
-                        else
-                                LeadIdCode = 0;
-                                XYZ = [0,0,0]; 
+        if (~tmp.flag1 | ~tmp.flag2 | ~tmp.flag3),
+        	if tmp.flag3;
+	                HDR.Label = cellstr(HDR.Label);
+	                NS = length(HDR.Label); 
+	        else
+	        	NS = length(HDR.LeadIdCode); 
+	        end;      
+
+                if tmp.flag3,
+	                if ~tmp.flag1,
+        	               HDR.ELEC.LOC   = repmat(NaN,NS,3);
+	        	end;
+                	if ~tmp.flag2,
+                       		HDR.LeadIdCode = repmat(NaN,NS,1);
+	        	end;
+                	for k = 1:NS;
+	                        ix = strmatch(upper(deblank(HDR.Label{k})),BIOSIG_GLOBAL.Label,'exact')
+        	                if (length(ix)==1),
+                	                LeadIdCode = BIOSIG_GLOBAL.LeadIdCode(ix)
+                        	        XYZ = BIOSIG_GLOBAL.XYZ(ix,:);
+                        	else	
+                                	LeadIdCode = 0;
+	                                XYZ = [0,0,0]; 
+        	                end;
+
+                	        if ~tmp.flag1,
+                        	        HDR.ELEC.XYZ(k,1:3) = XYZ;
+                        	end;
+
+                        	if ~tmp.flag2,
+                                	HDR.LeadIdCode(k,1) = LeadIdCode;
+	                        end;
                         end;
-                        
-                        if ~tmp.flag2
-                                HDR.LeadIdCode(k,1) = LeadIdCode;
-                        end;
-                        if ~tmp.flag1
-                                HDR.ELEC.XYZ(k,1:3) = XYZ;
-                        end;
+                else
+                       	HDR.Label = cell(NS,1);
+	               	for k = 1:NS;
+                        	ix = find(BIOSIG_GLOBAL.LeadIdCode==HDR.LeadIdCode(k));
+        	                if (length(ix)==1),
+	                                HDR.Label{k} = BIOSIG_GLOBAL.Label{ix};
+		                        if ~tmp.flag1,
+        		                        HDR.ELEC.LOC(k,1:3) = BIOSIG_GLOBAL.XYZ(ix,1:3);
+                		        end;
+               		        else
+               		        	HDR.Label{k} = ['#',int2str(k)];
+               		        end;
+	               	end;
                 end;
         end;
 end;
