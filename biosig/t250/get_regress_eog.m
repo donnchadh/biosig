ca877b2730087ae50de4a1cc1ec5eb280043ab91
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2007-03-22 13:06:18 +0000

    add REG+CAR, REG+ICA12 methods and FFT1-6Hz filter

--- biosig/t250/get_regress_eog.m
+++ biosig/t250/get_regress_eog.m
@@ -10,16 +10,21 @@ function [h0, s00] = get_regress_eog(fn,Mode)
 % INPUT: 
 %   file	filename which should be corrected
 %		usually, the eye movements are stored in a different file	
-%   Mode	'bf-'	beamformer, assume zero-activity reference electrode
-%		'bf+'	beamformer, take into account activity of reference electrode 
-%		'msec'	modified (without averaging) MSEC method [2]
+%   Mode	'REG'	[default] regression with one or two bipolar EOG channels
+%		'REG+CAR' regression and common average reference
+%			removes 2 bipolar + avergaged monopolar EOG
+%		'REG+PCA' regression and PCA, 
+%			removes 3 "EOG" components
+%		'REG+ICA' regression + ICA
+%			removes 3 "EOG" components
 %		'PCA-k'	removes the k-largest PCA components (k must be a positive integer)
-%		'REG'	[default] regression with one or two bipolar EOG channels
-%		'REG+PCA' regression and PCA, return 3 "EOG" components
-%		'REG+ICA' [not implemented]
-%		The following modifiers can be combined with any of the above	
+%		'msec'	same as PCA-3, modified (without averaging) MSEC method [2]
+%		'bf-'	beamformer, assume zero-activity reference electrode
+%		'bf+'	beamformer, take into account activity of reference electrode 
+%	The following modifiers can be combined with any of the above	
 %		'x'  	2nd player of season2 data
-%		'f16','+f16'	[default] filter 1-6hz
+%		'f16','+f16'	 FIR filter 1-6hz
+%		'fft16','+fft16' FFT filter 1-6hz
 %		'-f16'	turn filter off 
 %
 % OUTPUT:
@@ -44,7 +49,7 @@ function [h0, s00] = get_regress_eog(fn,Mode)
 %	A multiple source approach to the correction of eye artifacts.
 %	Electroencephalogr Clin Neurophysiol. 1994 Mar;90(3):229-41.
 
-%	$Id: get_regress_eog.m,v 1.4 2007-02-28 10:58:57 schloegl Exp $
+%	$Id: get_regress_eog.m,v 1.5 2007-03-22 13:06:18 schloegl Exp $
 %	Copyright (C) 2006,2007 by Alois Schloegl 
 %    	This is part of the BIOSIG-toolbox http://biosig.sf.net/
 
@@ -124,29 +129,53 @@ function [h0, s00] = get_regress_eog(fn,Mode)
 	        
 	        if size(s00,1)<h0.SampleRate*10,
 	        	fprintf(2,'WARNING GET_REGRESS_EOG: in %s no eye movements identified\n',h0.FileName);
-	        end; 	
+	        end;
 	end;
 
+	ix = strfind(Mode,'ICA-');
+	FLAG.ICA = ~isempty(ix);
+	if FLAG.ICA,
+		FLAG.ICA = str2double(strtok(Mode(ix+4:end),' '));
+	end;	
 	ix = strfind(Mode,'PCA-');
 	FLAG.PCA = ~isempty(ix);
 	if FLAG.PCA,
 		FLAG.PCA = str2double(strtok(Mode(ix+4:end),' '));
 	end;	
-	FLAG.MSEC = ~isempty(strfind(upper(Mode),'MSEC'));
-	if FLAG.MSEC,
-		FLAG.PCA=3;
-		FLAG.MSEC3 = ~isempty(strfind(upper(Mode),'MSEC3'));
+	if ~isempty(strfind(upper(Mode),'MSEC'));
+		FLAG.PCA = 3;
 	end;
 	
 	% 	filtering the data should improve the estimated        
-	FLAG.FILTER_16 = isempty(strfind(Mode,'-f16'));
+	FLAG.FILTER_FFT16 = ~isempty(strfind(Mode,'fft16'));
+	FLAG.FILTER_16 = ~isempty(strfind(Mode,'f16'));
 	if FLAG.FILTER_16,	
 	        b = fir1(h0.SampleRate,[1,6]/h0.SampleRate*2); a=1;
-        else
-		b = 1; a = 1;
 	end;
 	FLAG.REG = ~isempty(strfind(upper(Mode),'REG'));
         
+	%%%%% define EEG and EOG channels %%%%%
+	CHANTYP = repmat(' ',h0.NS,1);
+	CHANTYP([(h0.LeadIdCode>=996) & (h0.LeadIdCode<=1302)])='E'; % EEG
+ 	eogchan = identify_eog_channels(h0); 
+        CHANTYP(any(eogchan,2)) = 'O';	% EOG
+      	if FLAG.SEASON2DATA_PLAYER2,	
+	        CHANTYP = CHANTYP([65:128,1:64]);
+	        eogchan = eogchan([65:128,1:64],:);
+	end;
+
+	% filter data  
+	if 0,
+	elseif FLAG.FILTER_FFT16,
+		tmp = center(s00,1); tmp(isnan(tmp)) = 0;
+	        tmp = fft(tmp); f=[0:size(s00,1)-1]*h0.SampleRate/size(s00,1);
+	        w = ((f>1) & (f<6)) | ((f>h0.SampleRate-6) & (f<h0.SampleRate-1));
+	        tmp(~w,:) = 0;
+	        s00 = real(ifft(tmp));
+	elseif FLAG.FILTER_16,
+	        s00 = filter(b,a,s00);
+	end;        
+
 	%%%%% identify EOG components %%%%%	
 	if 0,
 	
@@ -186,76 +215,76 @@ function [h0, s00] = get_regress_eog(fn,Mode)
 			eogchan = eogchan([h0.NS/2+1:h0.NS,1:h0.NS/2],:);
 		end;
 
-	elseif FLAG.PCA,
-		% identify EOG channels
-	        eogchan = identify_eog_channels(h0); 
-
+	elseif (FLAG.PCA>0) | (FLAG.ICA>0),
 		% identify channels used for PCA
 		if isempty(strfind(upper(Mode),'MSEC1'))
 			% MSEC,MSEC2 [default]
-		        chan = find([(h0.LeadIdCode>=996) & (h0.LeadIdCode<=1302)] | any(eogchan,2));
+			chan = find((CHANTYP=='E') | (CHANTYP=='O'));
 		else  % MSEC1 
-		        chan = find((h0.LeadIdCode>=996) & (h0.LeadIdCode<=1302));
+			chan = find(CHANTYP=='E');
 		end;
 	        nc      = length(chan); 
 	        chan    = sparse(chan,1:nc,1,h0.NS,nc)*(eye(nc) - 1/nc); % Common Average Reference (CAR)
 		r       = [chan,eogchan];  % CAR and bipolar EOG
-		if FLAG.SEASON2DATA_PLAYER2,
-			r = r([h0.NS/2+1:h0.NS,1:h0.NS/2],:);
-		end;
 
-		tmp = filter(b,a,center(s00,1)*r);
+		tmp = s00*r; 
 		tmp = tmp(~any(isnan(tmp),2),:); 
-		[u,s,eogchan] = svds(tmp, FLAG.PCA); % get k (i.e. FLAG.PCA) largests PCA's
-		eogchan = r*eogchan;
-		h0.pca.v= (diag(s).^2)/sum(tmp(:).^2);
-		%h0.SVDS.SSE = sum(tmp(:).^2);
+		if FLAG.PCA,
+			[u,s,ochan] = svds(tmp, FLAG.PCA); % get k (i.e. FLAG.PCA) largests PCA's
+		elseif FLAG.ICA
+			[A] = jade(tmp', FLAG.ICA); % get k (i.e. FLAG.PCA) largests PCA's
+			ochan = pinv(A)';
+		end;
+		eogchan = r*ochan;
 		 
 	elseif FLAG.REG, %REGRESSION
 	        % define xEOG channels
 	        FLAG.REG = 1; 
-	        if FLAG.SEASON2DATA_PLAYER2,	
-		        eogchan = identify_eog_channels(h0,'x'); 
-		else        
-			eogchan = identify_eog_channels(h0); 
+		if ~isempty(strfind(upper(Mode),'REG+CAR1'));
+	        	eogchan = [eogchan,any(eogchan,2)];
+		elseif ~isempty(strfind(upper(Mode),'REG+CAR2'));
+			eogchan = [eogchan,double([CHANTYP=='O']/sum(CHANTYP=='O')-[CHANTYP>' ']/sum(CHANTYP>' '))];
 		end;
-	        %FLAG.REG = size(eogchan,2); % number of components
 	end;        
-	end;
 
        	% remove EOG channels for list of corrected channels
-	chan  = 1:h0.NS;
-	%chan = find(~any(eogchan,2)); 
-
+	% chan = 1:h0.NS;
+	chan = find(CHANTYP=='E'); 
         % regression analysis - compute correction coefficients. 
-	[h0.REGRESS, s01] = regress_eog(filter(b,a,s00),chan,eogchan); 
-
+	[h0.REGRESS, s01] = regress_eog(s00,chan,eogchan); 
+	h0.REGRESS.FLAG = FLAG;	
 
 	%%%%% post-regression improvement
-	if ~isempty(strfind(Mode,'REG+'))	
+	if length(Mode)>6,
+	if ~isempty(strfind(Mode([1:4,6,7]),'REG+CA'))	
 		% select EEG and EOG channels and do CAR
-	        echan = find([(h0.LeadIdCode>=996) & (h0.LeadIdCode<=1302)] | any(eogchan,2));
+		echan = find(CHANTYP>' ');
 	        nc    = length(echan); 
 	        echan = sparse(echan,1:nc,1,h0.NS,nc)*(eye(nc) - 1/nc); % Common Average Reference (CAR)
 
-		if FLAG.SEASON2DATA_PLAYER2,
-			echan = echan([h0.NS/2+1:h0.NS,1:h0.NS/2],:);
-		end;
-
+		tmp = strtok(Mode)
+		nc = str2double(tmp(8:end));
+		if isempty(nc) | isnan(nc),
+			nc = 1;
+		else
+		end;		
         	tmp = center(s01,1)*echan;
 		tmp = tmp(~any(isnan(tmp),2),:); 
 		if ~isempty(strfind(Mode,'REG+PCA'))	
-			 % get largests PCA's
-			[u,s,zeog] = svds(tmp, 3-size(eogchan,2));
-			
-	        elseif 0, ~isempty(strfind(Mode,'REG+ICA'))	
-			%%% ICA: not implemented, yet		
-			
+			% get largest PCA's
+			[u,s,zeog] = svds(tmp, nc);
+	        elseif ~isempty(strfind(Mode,'REG+ICA'))
+	        	%%% ICA: identify one component
+			[A,s] = jade(tmp',nc);
+			zeog  = pinv(A)';
 		else 	
-			fprintf(2,'Mode %s not supported - used regression only\n',Mode); 
+			fprintf(2,'Mode %s not supported - regression is used only.\n',Mode); 
 			return;
 		end; 
 		
-        	[h0.REGRESS,s01] = regress_eog(filter(b,a,s00),chan,[eogchan,echan*zeog]); 
+		w = sparse([eogchan,h0.REGRESS.r0*echan*zeog]); 
+		[h0.REGRESS,s01] = regress_eog(s00,chan,w);
 	end
-	
+	end
+	h0.REGRESS.FLAG = FLAG;	
+
