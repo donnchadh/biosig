668ebaf173b4db44a3b1eb2af758a756e7d5ef12
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2007-05-24 10:21:22 +0000

    include licences and copyright

--- biosig4c++/save2gdf.c
+++ biosig4c++/save2gdf.c
@@ -1,3 +1,28 @@
+/*
+
+    $Id: save2gdf.c,v 1.2 2007-05-24 10:21:22 schloegl Exp $
+    Copyright (C) 2000,2005,2007 Alois Schloegl <a.schloegl@ieee.org>
+    Copyright (C) 2007 Elias Apostolopoulos
+    This function is part of the "BioSig for C/C++" repository 
+    (biosig4c++) at http://biosig.sf.net/ 
+
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "biosig.h"
@@ -39,12 +64,12 @@ int main(int argc, char **argv){
 */	
     for (int k=0; k<hdr->NS; k++) {
 	cp = hdr->CHANNEL+k; 
-	fprintf(stdout,"\n#%2i: %7s\t%s\t%s\t%i\t%5f\t%5f\t%5f\t%5f\t",k,cp->Label,cp->Transducer,cp->PhysDim,cp->PhysDimCode,cp->PhysMax,cp->PhysMin,cp->DigMax,cp->DigMin);
+	fprintf(stdout,"\n#%2i: %7s\t%s\t%s\t%i\t%5f\t%5f\t%5f\t%5f\t%5f\t",k,cp->Label,cp->Transducer,cp->PhysDim,cp->PhysDimCode,cp->PhysMax,cp->PhysMin,cp->DigMax,cp->DigMin,cp->Cal);
 	fprintf(stderr,"\n%4.0f\t%4.0f\t%4.0f\t%5f Ohm",cp->LowPass,cp->HighPass,cp->Notch,cp->Impedance);
     }
     fprintf(stderr,"\nm1: %d %d %d %d\n",*((int16_t *)hdr->AS.rawdata),*((int16_t *)hdr->AS.rawdata+2),*(int16_t *)(hdr->AS.rawdata+4),*(int16_t *)(hdr->AS.rawdata+6));
 
-    fprintf(stderr,"\nSCP OPENED: SUCCESSFULLY %i\n",ftell(hdr->FILE.FID));
+    fprintf(stderr,"\nFile %s read successfully %i\n",hdr->FileName,ftell(hdr->FILE.FID));
     hdr->FLAG.OVERFLOWDETECTION = 0;
     hdr->FLAG.UCAL = 1;
     
@@ -58,8 +83,8 @@ int main(int argc, char **argv){
     hdr->TYPE = SCP_ECG;
     hdr->TYPE = HL7aECG;
     hdr->TYPE = GDF;
-    sopen(argv[2], "w", hdr);
-    fprintf(stderr,"File %s: OPENED SUCCESSFULLY\n", hdr->FileName);
+    hdr = sopen(argv[2], "w", hdr);
+    fprintf(stderr,"File %s : sopen-write\n", hdr->FileName);
 
 #if __BYTE_ORDER == __BIG_ENDIAN
 		// fix endianity of the data
--- biosig4c++/save2scp.c
+++ biosig4c++/save2scp.c
@@ -1,3 +1,27 @@
+/*
+
+    $Id: save2scp.c,v 1.2 2007-05-24 10:21:22 schloegl Exp $
+    Copyright (C) 2000,2005 Alois Schloegl <a.schloegl@ieee.org>
+    Copyright (C) 2007 Elias Apostolopoulos
+    This function is part of the "BioSig for C/C++" repository 
+    (biosig4c++) at http://biosig.sf.net/ 
+
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include "biosig.h"
@@ -39,7 +63,7 @@ int main(int argc, char **argv){
 */	
     for (int k=0; k<hdr->NS; k++) {
 	cp = hdr->CHANNEL+k; 
-	fprintf(stdout,"\n#%2i: %7s\t%s\t%s\t%i\t%5f\t%5f\t%5f\t%5f\t",k,cp->Label,cp->Transducer,cp->PhysDim,cp->PhysDimCode,cp->PhysMax,cp->PhysMin,cp->DigMax,cp->DigMin);
+	fprintf(stdout,"\n#%2i: %7s\t%s\t%s\t%i\t%5f\t%5f\t%5f\t%5f\t%5f\t",k,cp->Label,cp->Transducer,cp->PhysDim,cp->PhysDimCode,cp->PhysMax,cp->PhysMin,cp->DigMax,cp->DigMin,cp->Cal);
 	fprintf(stderr,"\n%4.0f\t%4.0f\t%4.0f\t%5f Ohm",cp->LowPass,cp->HighPass,cp->Notch,cp->Impedance);
     }
     fprintf(stderr,"\nm1: %d %d %d %d\n",*((int16_t *)hdr->AS.rawdata),*((int16_t *)hdr->AS.rawdata+2),*(int16_t *)(hdr->AS.rawdata+4),*(int16_t *)(hdr->AS.rawdata+6));
