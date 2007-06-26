3d44eef4f7af35861e5521d6e9faa718b57ece3d
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2007-06-26 23:56:20 +0000

    use SWRITE for writing data; additional header checks on Phys/Dig/Min/Max

--- biosig4c++/save2gdf.c
+++ biosig4c++/save2gdf.c
@@ -1,6 +1,6 @@
 /*
 
-    $Id: save2gdf.c,v 1.4 2007-06-17 19:57:30 schloegl Exp $
+    $Id: save2gdf.c,v 1.5 2007-06-26 23:56:20 schloegl Exp $
     Copyright (C) 2000,2005,2007 Alois Schloegl <a.schloegl@ieee.org>
     Copyright (C) 2007 Elias Apostolopoulos
     This function is part of the "BioSig for C/C++" repository 
@@ -47,7 +47,7 @@ int main(int argc, char **argv){
     {
     	if (!strcmp(argv[1],"-v") | !strcmp(argv[1],"--version") )
     	{
-		fprintf(stdout,"save2gdf (biosig4c++) 0.40+\n");
+		fprintf(stdout,"save2gdf (biosig4c++) 0.42+\n");
 		fprintf(stdout,"Written by Alois Schloegl and others\n\n");
 		fprintf(stdout,"This is free software.\n");
 		return(0);
@@ -61,7 +61,7 @@ int main(int argc, char **argv){
 		fprintf(stdout,"   -h, --help   \n\tprints this information\n");
 		fprintf(stdout,"   -f=FMT  \n\tconverts data into format FMT\n");
 		fprintf(stdout,"\tFMT must represent and valid target file format\n"); 
-		fprintf(stdout,"\tCurrently are supported: HL7aECG, SCP_ECG(EN1064), GDF, EDF\n"); 
+		fprintf(stdout,"\tCurrently are supported: HL7aECG, SCP_ECG(EN1064), GDF, EDF, BDF\n"); 
 		fprintf(stdout,"\n\n");
 		return(0);
 	}	
@@ -70,6 +70,8 @@ int main(int argc, char **argv){
 			TARGET_TYPE=GDF;
     		else if (!strcmp(argv[1],"-f=EDF"))
 			TARGET_TYPE=EDF;
+    		else if (!strcmp(argv[1],"-f=BDF"))
+			TARGET_TYPE=BDF;
     		else if (!strncmp(argv[1],"-f=HL7",6) )
 			TARGET_TYPE=HL7aECG;
     		else if (!strncmp(argv[1],"-f=SCP",6))
@@ -93,9 +95,8 @@ int main(int argc, char **argv){
     hdr = sopen(source, "r", NULL);
     
     if (hdr==NULL)exit(-1);
-
     fprintf(stderr,"FileName:\t%s\nType    :\t%i\nVersion:\t%4.2f\nHeadLen:\t%i\n",source,hdr->TYPE,hdr->VERSION,hdr->HeadLen);
-    fprintf(stderr,"NS:\t%i\nSPR:\t%i\nNRec:\t%Li\nDuration[s]:\t%u/%u\nFs:\t%f\n",hdr->NS,hdr->SPR,hdr->NRec,hdr->Dur[0],hdr->Dur[1],hdr->SampleRate);
+    fprintf(stderr,"NS:\t%i\nSPR:\t%i\nNRec:\t%Li\nSpB:\t%i\nDuration[s]:\t%u/%u\nFs:\t%f\n",hdr->NS,hdr->SPR,hdr->NRec,hdr->AS.bpb,hdr->Dur[0],hdr->Dur[1],hdr->SampleRate);
 
     T0 = gdf_time2t_time(hdr->T0);
     fprintf(stderr,"Date/Time:\t%s\n",asctime(localtime(&T0))); 
@@ -105,14 +106,14 @@ int main(int argc, char **argv){
     fprintf(stderr,"Patient:\n\tName:\t%s\n\tId:\t%s\n\tWeigth:\t%i kg\n\tHeigth:\t%i cm\n\tAge:\t%4.1f y\n",hdr->Patient.Name,hdr->Patient.Id,hdr->Patient.Weight,hdr->Patient.Height,(hdr->T0 - hdr->Patient.Birthday)/ldexp(365.25,32)); 
     T0 = gdf_time2t_time(hdr->Patient.Birthday);
     fprintf(stderr,"\tBirthday:\t%s\n",asctime(localtime(&T0))); 
-/*  fprintf(stdout,"EVENT:\n\tN:\t%i\n\tFs:\t%f\n\t\n",hdr->EVENT.N,hdr->EVENT.SampleRate); 
+    fprintf(stdout,"EVENT:\n\tN:\t%i\n\tFs:\t%f\n\t\n",hdr->EVENT.N,hdr->EVENT.SampleRate); 
 		
     fprintf(stdout,"--%i\t%i\n", hdr->FLAG.OVERFLOWDETECTION, hdr->FLAG.UCAL);
     hdr->FLAG.OVERFLOWDETECTION = 0; 
 	//	hdr->FLAG.UCAL = 1;
     fprintf(stdout,"--%i\t%i\n", hdr->FLAG.OVERFLOWDETECTION, hdr->FLAG.UCAL);
     fprintf(stdout,"2-%u\t%i\t%i\t%i\t%u\t%u\n",hdr->AS.bpb,hdr->FILE.OPEN,(int32_t)hdr->NRec,hdr->HeadLen,hdr->Dur[0],hdr->Dur[1]);
-*/
+
     for (int k=0; k<hdr->NS; k++) {
 	cp = hdr->CHANNEL+k; 
 	fprintf(stdout,"\n#%2i: %7s\t%s\t%s\t%i\t%5f\t%5f\t%5f\t%5f\t%5f\t",k,cp->Label,cp->Transducer,cp->PhysDim,cp->PhysDimCode,cp->PhysMax,cp->PhysMin,cp->DigMax,cp->DigMin,cp->Cal);
@@ -131,7 +132,64 @@ int main(int argc, char **argv){
 	hdr->FILE.FID = 0;
     }
 
-    hdr->TYPE = TARGET_TYPE;
+   /********************************* 
+   	Write data 
+   *********************************/
+
+    	hdr->TYPE = TARGET_TYPE;
+    	// header conditioning
+	double PhysMax = hdr->data.block[0];
+	double PhysMin = hdr->data.block[0];
+    	for (int k=0; k<hdr->NS; k++) {
+	/*    	
+		hdr->CHANNEL[k].PhysMax = hdr->CHANNEL[k].DigMax * hdr->CHANNEL[k].Cal + hdr->CHANNEL[k].Off; 
+		hdr->CHANNEL[k].PhysMin = hdr->CHANNEL[k].DigMin * hdr->CHANNEL[k].Cal + hdr->CHANNEL[k].Off; 
+	*/
+		uint32_t N=hdr->NRec*hdr->SPR;
+		hdr->CHANNEL[k].PhysMax = hdr->data.block[k*N];
+		hdr->CHANNEL[k].PhysMin = hdr->data.block[k*N];
+		for (uint32_t k1=1; k1<N; k1++) {
+			if (hdr->CHANNEL[k].PhysMax < hdr->data.block[k*N+k1])
+			 	hdr->CHANNEL[k].PhysMax = hdr->data.block[k*N+k1];
+		 	if (hdr->CHANNEL[k].PhysMin > hdr->data.block[k*N+k1])
+		 		hdr->CHANNEL[k].PhysMin = hdr->data.block[k*N+k1];   		
+		}
+		
+		if (hdr->TYPE==SCP_ECG) {
+			if (PhysMax < hdr->CHANNEL[k].PhysMax)
+		 		PhysMax = hdr->CHANNEL[k].PhysMax;   		
+			if (PhysMin > hdr->CHANNEL[k].PhysMin)
+		 		PhysMax = hdr->CHANNEL[k].PhysMax;   		
+    		}
+		else if (hdr->TYPE==EDF)	{
+			hdr->CHANNEL[k].DigMax = ldexp(1.0,15)-1.0; 
+			hdr->CHANNEL[k].DigMin = ldexp(-1.0,15); 
+    		}
+		else if (hdr->TYPE==BDF) {
+			hdr->CHANNEL[k].DigMax = ldexp(1.0,23)-1.0; 
+			hdr->CHANNEL[k].DigMin = ldexp(-1.0,23); 
+    		}
+    		else if (hdr->TYPE==GDF) {
+    			// ### Phys/Dig/Max/Min depend on GDFTYP
+			hdr->CHANNEL[k].GDFTYP = 16;
+			hdr->CHANNEL[k].DigMax = hdr->CHANNEL[k].PhysMax; 
+			hdr->CHANNEL[k].DigMin = hdr->CHANNEL[k].PhysMin; 
+    		}
+	}
+	double Cal1 = PhysMax/(ldexp(1.0,15)-1.0);
+	double Cal2 = PhysMin/ldexp(-1.0,15);
+	double Cal = (Cal1 > Cal2 ? Cal1 : Cal2);
+    	for (int k=0; k<hdr->NS; k++) {
+		if (hdr->TYPE==SCP_ECG) {
+			hdr->CHANNEL[k].Cal = Cal;
+			hdr->CHANNEL[k].Off = 0.0;
+		}
+		else {
+			hdr->CHANNEL[k].Cal = (hdr->CHANNEL[k].PhysMax-hdr->CHANNEL[k].PhysMin)/(hdr->CHANNEL[k].DigMax-hdr->CHANNEL[k].DigMin);
+			hdr->CHANNEL[k].Off =  hdr->CHANNEL[k].PhysMin-hdr->CHANNEL[k].Cal * hdr->CHANNEL[k].DigMin;
+		}
+	}
+    
     hdr = sopen(dest, "w", hdr);
     fprintf(stderr,"File %s : sopen-write\n", hdr->FileName);
     if ( (hdr->TYPE != SCP_ECG) & (hdr->TYPE != HL7aECG) ) /* SCP_ECG and HL7aECG write data during SOPEN */
@@ -144,7 +202,8 @@ int main(int argc, char **argv){
 	}
 #endif 
 	// write data 
-	fwrite(hdr->AS.rawdata, 4 ,hdr->NRec*hdr->SPR*hdr->NS, hdr->FILE.FID);
+	//fwrite(hdr->AS.rawdata, 4 ,hdr->NRec*hdr->SPR*hdr->NS, hdr->FILE.FID);
+	swrite(hdr->AS.rawdata, hdr->NRec*hdr->SPR*hdr->NS, hdr);
     }
     sclose(hdr);
     free(hdr);
