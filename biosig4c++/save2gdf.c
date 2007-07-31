2915156eb8a4e3fa7c5b09a386d644eb489babd3
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2007-07-31 20:24:32 +0000

    remove obsolete code

--- biosig4c++/save2gdf.c
+++ biosig4c++/save2gdf.c
@@ -1,6 +1,6 @@
 /*
 
-    $Id: save2gdf.c,v 1.11 2007-07-29 21:41:47 schloegl Exp $
+    $Id: save2gdf.c,v 1.12 2007-07-31 20:24:32 schloegl Exp $
     Copyright (C) 2000,2005,2007 Alois Schloegl <a.schloegl@ieee.org>
     Copyright (C) 2007 Elias Apostolopoulos
     This function is part of the "BioSig for C/C++" repository 
@@ -76,6 +76,8 @@ int main(int argc, char **argv){
 			TARGET_TYPE=HL7aECG;
     		else if (!strncmp(argv[1],"-f=SCP",6))
 			TARGET_TYPE=SCP_ECG;
+    		else if (!strncmp(argv[1],"-f=CFWB",6))
+			TARGET_TYPE=CFWB;
 		else {
 			fprintf(stderr,"format %s not supported.\n",argv[1]);
 			return(-1);
@@ -96,173 +98,92 @@ int main(int argc, char **argv){
     	}	
 
     
-    hdr = sopen(source, "r", NULL);
+	hdr = sopen(source, "r", NULL);
     
-    if (hdr==NULL) exit(-1);
-    fprintf(stderr,"FileName:\t%s\nType    :\t%i\nVersion:\t%4.2f\nHeadLen:\t%i\n",source,hdr->TYPE,hdr->VERSION,hdr->HeadLen);
-    fprintf(stderr,"NS:\t%i\nSPR:\t%i\nNRec:\t%Li\nSpB:\t%i\nDuration[s]:\t%u/%u\nFs:\t%f\n",hdr->NS,hdr->SPR,hdr->NRec,hdr->AS.bpb,hdr->Dur[0],hdr->Dur[1],hdr->SampleRate);
+	if (hdr==NULL) exit(-1);
+	fprintf(stdout,"FileName:\t%s\nType    :\t%i\nVersion:\t%4.2f\nHeadLen:\t%i\n",source,hdr->TYPE,hdr->VERSION,hdr->HeadLen);
+	fprintf(stdout,"NS:\t%i\nSPR:\t%i\nNRec:\t%Li\nSpB:\t%i\nDuration[s]:\t%u/%u\nFs:\t%f\n",hdr->NS,hdr->SPR,hdr->NRec,hdr->AS.bpb,hdr->Dur[0],hdr->Dur[1],hdr->SampleRate);
 
-    T0 = gdf_time2t_time(hdr->T0);
-    fprintf(stderr,"Date/Time:\t%s\n",asctime(localtime(&T0))); 
+	T0 = gdf_time2t_time(hdr->T0);
+	fprintf(stdout,"Date/Time:\t%s\n",asctime(localtime(&T0))); 
 		//T0 = gdf_time2t_time(hdr->Patient.Birthday);
 		//fprintf(stdout,"Birthday:\t%s\n",asctime(localtime(&T0)));
 
-    fprintf(stderr,"Patient:\n\tName:\t%s\n\tId:\t%s\n\tWeigth:\t%i kg\n\tHeigth:\t%i cm\n\tAge:\t%4.1f y\n",hdr->Patient.Name,hdr->Patient.Id,hdr->Patient.Weight,hdr->Patient.Height,(hdr->T0 - hdr->Patient.Birthday)/ldexp(365.25,32)); 
-    T0 = gdf_time2t_time(hdr->Patient.Birthday);
-    fprintf(stderr,"\tBirthday:\t%s\n",asctime(localtime(&T0))); 
-    fprintf(stdout,"EVENT:\n\tN:\t%i\n\tFs:\t%f\n",hdr->EVENT.N,hdr->EVENT.SampleRate); 
-		
-    for (int k=0; k<hdr->NS; k++) {
-	cp = hdr->CHANNEL+k; 
+	fprintf(stdout,"Patient:\n\tName:\t%s\n\tId:\t%s\n\tWeigth:\t%i kg\n\tHeigth:\t%i cm\n\tAge:\t%4.1f y\n",hdr->Patient.Name,hdr->Patient.Id,hdr->Patient.Weight,hdr->Patient.Height,(hdr->T0 - hdr->Patient.Birthday)/ldexp(365.25,32)); 
+	T0 = gdf_time2t_time(hdr->Patient.Birthday);
+	fprintf(stdout,"\tBirthday:\t%s\n",asctime(localtime(&T0))); 
+	fprintf(stdout,"EVENT:\n\tN:\t%i\n\tFs:\t%f\n",hdr->EVENT.N,hdr->EVENT.SampleRate); 
+
+	for (int k=0; k<hdr->NS; k++) {
+		cp = hdr->CHANNEL+k; 
 /*	fprintf(stdout,"\n#%2i: %7s\t%s\t%s\t%i\t%5f\t%5f\t%5f\t%5f\t%5f\t",k,cp->Label,cp->Transducer,cp->PhysDim,cp->PhysDimCode,cp->PhysMax,cp->PhysMin,cp->DigMax,cp->DigMin,cp->Cal);
 	fprintf(stderr,"\n%4.0f\t%4.0f\t%4.0f\t%5f Ohm",cp->LowPass,cp->HighPass,cp->Notch,cp->Impedance);
 */
-	fprintf(stdout,"\n#%2i: %7s\t%5f %5f %s\t%i\t%5f\t%5f\t%5f\t%5f\t",k,cp->Label,cp->Cal,cp->Off,cp->PhysDim,cp->PhysDimCode,cp->PhysMax,cp->PhysMin,cp->DigMax,cp->DigMin);
-    }
-    fprintf(stdout,"\nm1: %d %d %d %d\n",*((int16_t *)hdr->AS.rawdata),*((int16_t *)hdr->AS.rawdata+2),*(int16_t *)(hdr->AS.rawdata+4),*(int16_t *)(hdr->AS.rawdata+6));
+		fprintf(stdout,"\n#%2i: %7s\t%5f %5f %s\t%i\t%5f\t%5f\t%5f\t%5f",k,cp->Label,cp->Cal,cp->Off,cp->PhysDim,cp->PhysDimCode,cp->PhysMax,cp->PhysMin,cp->DigMax,cp->DigMin);
+	}
+	fprintf(stdout,"\nm1: %d %d %d %d\n",*((int16_t *)hdr->AS.rawdata),*((int16_t *)hdr->AS.rawdata+2),*(int16_t *)(hdr->AS.rawdata+4),*(int16_t *)(hdr->AS.rawdata+6));
 
-    hdr->FLAG.OVERFLOWDETECTION = 0;
-    hdr->FLAG.UCAL = 1;
+	hdr->FLAG.OVERFLOWDETECTION = 0;
+	hdr->FLAG.UCAL = 1;
     
-    count = sread(hdr, 0, hdr->NRec);
+	count = sread(hdr, 0, hdr->NRec);
 
-    fprintf(stdout,"\nFile %s read successfully %i %i\n",hdr->FileName,hdr->EVENT.N,hdr->SPR);
-
-//    fprintf(stdout,"%i\t%i\n", hdr->data.block[0], hdr->data.block[hdr->SPR]);
-//    fprintf(stdout,"%i\t%i\n", hdr->data.block[1], hdr->data.block[hdr->SPR+1]);
-
+	fprintf(stdout,"\nFile %s read successfully %i %i %i\n",hdr->FileName,hdr->EVENT.N,hdr->SPR,hdr->NS);
 	if (dest==NULL) {
 		sclose(hdr);
 		free(hdr);
 		return(0);
 	}
 
-    if (hdr->FILE.OPEN){
-#ifdef ZLIB_H
-	gzclose(hdr->FILE.FID);
-#else
-	fclose(hdr->FILE.FID);
-#endif
-	hdr->FILE.FID = 0;
-	free(hdr->AS.Header1);
-	hdr->AS.Header1 = NULL;
-    }
-
-    fprintf(stdout,"\nFile %s closed \n",hdr->FileName);
+	if (hdr->FILE.OPEN){
+		FCLOSE(hdr->FILE.FID);
+		hdr->FILE.FID = 0;
+		free(hdr->AS.Header1);
+		hdr->AS.Header1 = NULL;
+	}
+	fprintf(stdout,"\nFile %s closed \n",hdr->FileName);
 
    /********************************* 
    	Write data 
    *********************************/
 
     	hdr->TYPE = TARGET_TYPE;
+	if (hdr->TYPE==GDF || hdr->TYPE==CFWB) {
+		size_t N = hdr->NRec*hdr->SPR;
+    		for (int k=0; k<hdr->NS; k++) {
+			double MaxValue = hdr->data.block[k*N];
+			double MinValue = hdr->data.block[k*N];
+			/* Maximum and Minimum for channel k */ 
+			for (uint32_t k1=1; k1<N; k1++) {
+				if (MaxValue < hdr->data.block[k*N+k1])
+			 		MaxValue = hdr->data.block[k*N+k1];
+		 		if (MinValue > hdr->data.block[k*N+k1])
+		 			MinValue = hdr->data.block[k*N+k1];
+			}
+	    		if (!hdr->FLAG.UCAL) {
+	    			MaxValue = (MaxValue - hdr->CHANNEL[k].Off)/hdr->CHANNEL[k].Cal;
+	    			MinValue = (MinValue - hdr->CHANNEL[k].Off)/hdr->CHANNEL[k].Cal;
+	    		}
 
-	size_t N=hdr->NRec*hdr->SPR;
-    	for (int k=0; k<hdr->NS; k++) {
-		double PhysMax = hdr->data.block[k*N];
-		double PhysMin = hdr->data.block[k*N];
-    fprintf(stdout,"\nFile %s closed %i\n",hdr->FileName,k);
-		/* Maximum and Minimum for channel k */ 
-		for (uint32_t k1=1; k1<N; k1++) {
-			if (PhysMax < hdr->data.block[k*N+k1])
-			 	PhysMax = hdr->data.block[k*N+k1];
-		 	if (PhysMin > hdr->data.block[k*N+k1])
-		 		PhysMin = hdr->data.block[k*N+k1];   		
-		}
-
-	    	if ((hdr->FLAG.UCAL == 1) && (hdr->TYPE==GDF))
-		{
 			/* heuristic to determine optimal data type */
-			if ((PhysMax <= 127) && (PhysMin >= -128))
+			if ((MaxValue <= 127) && (MinValue >= -128))
 		    		hdr->CHANNEL[k].GDFTYP = 1;
-			else if ((PhysMax <= 255.0) && (PhysMin >= 0.0))
+			else if ((MaxValue <= 255.0) && (MinValue >= 0.0))
 			    	hdr->CHANNEL[k].GDFTYP = 2;
-			else if ((PhysMax <= ldexp(1.0,15)-1.0) && (PhysMin >= ldexp(-1.0,15)))
+			else if ((MaxValue <= ldexp(1.0,15)-1.0) && (MinValue >= ldexp(-1.0,15)))
 		    		hdr->CHANNEL[k].GDFTYP = 3;
-			else if ((PhysMax <= ldexp(1.0,16)-1.0) && (PhysMin >= 0.0))
+			else if ((MaxValue <= ldexp(1.0,16)-1.0) && (MinValue >= 0.0))
 			    	hdr->CHANNEL[k].GDFTYP = 4;
-			else if ((PhysMax <= ldexp(1.0,31)-1.0) && (PhysMin >= ldexp(-1.0,31)))
+			else if ((MaxValue <= ldexp(1.0,31)-1.0) && (MinValue >= ldexp(-1.0,31)))
 		    		hdr->CHANNEL[k].GDFTYP = 5;
-			else if ((PhysMax <= ldexp(1.0,32)-1.0) && (PhysMin >= 0.0))
+			else if ((MaxValue <= ldexp(1.0,32)-1.0) && (MinValue >= 0.0))
 		    		hdr->CHANNEL[k].GDFTYP = 6;
-		}    		
-	}
-	
-
-if (0) { // ### MAYBE OBSOLETE ### 
-    	// header conditioning
-	double PhysMax = hdr->data.block[0];
-	double PhysMin = hdr->data.block[0];
-    	for (int k=0; k<hdr->NS; k++) {
-	/*    	
-		hdr->CHANNEL[k].PhysMax = hdr->CHANNEL[k].DigMax * hdr->CHANNEL[k].Cal + hdr->CHANNEL[k].Off; 
-		hdr->CHANNEL[k].PhysMin = hdr->CHANNEL[k].DigMin * hdr->CHANNEL[k].Cal + hdr->CHANNEL[k].Off; 
-	*/
-		uint32_t N=hdr->NRec*hdr->SPR;
-		hdr->CHANNEL[k].PhysMax = hdr->data.block[k*N];
-		hdr->CHANNEL[k].PhysMin = hdr->data.block[k*N];
-		for (uint32_t k1=1; k1<N; k1++) {
-			if (hdr->CHANNEL[k].PhysMax < hdr->data.block[k*N+k1])
-			 	hdr->CHANNEL[k].PhysMax = hdr->data.block[k*N+k1];
-		 	if (hdr->CHANNEL[k].PhysMin > hdr->data.block[k*N+k1])
-		 		hdr->CHANNEL[k].PhysMin = hdr->data.block[k*N+k1];   		
-		}
-
-		if (0)
-		 	;
-		else if (hdr->TYPE==SCP_ECG) {
-/*			
-fprintf(stdout,"SOPEN-SCP-W2: #%i %e\n",k,hdr->CHANNEL[k].Cal);
-			hdr->CHANNEL[k].Cal *= PhysDimScale(hdr->CHANNEL[k].PhysDimCode)*1e9; 
-fprintf(stdout,"SOPEN-SCP-W3: #%i %e\n",k,hdr->CHANNEL[k].Cal);
-			hdr->CHANNEL[k].PhysDimCode = 4276; //PhysDimCode("nV"); 
-fprintf(stdout,"SOPEN-SCP-W4: %i.\n",k);
-			if (PhysMax < hdr->CHANNEL[k].PhysMax)
-		 		PhysMax = hdr->CHANNEL[k].PhysMax;   		
-			if (PhysMin > hdr->CHANNEL[k].PhysMin)
-		 		PhysMin = hdr->CHANNEL[k].PhysMin;   		
-*/
-    		}
-		else if (hdr->TYPE==EDF)	{
-			hdr->CHANNEL[k].DigMax = ldexp(1.0,15)-1.0; 
-			hdr->CHANNEL[k].DigMin = ldexp(-1.0,15); 
-    		}
-		else if (hdr->TYPE==BDF) {
-			hdr->CHANNEL[k].DigMax = ldexp(1.0,20)-1.0; 
-			hdr->CHANNEL[k].DigMin = ldexp(-1.0,20); 
-    		}
-    		else if (hdr->TYPE==GDF) {
-    			// ### Phys/Dig/Max/Min depend on GDFTYP
-			hdr->CHANNEL[k].GDFTYP = 16;
-			hdr->CHANNEL[k].DigMax = hdr->CHANNEL[k].PhysMax; 
-			hdr->CHANNEL[k].DigMin = hdr->CHANNEL[k].PhysMin; 
-    		}
-	}
-	
-	double Cal1 = PhysMax/(ldexp(1.0,15)-1.0);
-	double Cal2 = PhysMin/ldexp(-1.0,15);
-	double Cal  = (Cal1 > Cal2 ? Cal1 : Cal2);
-    	for (int k=0; k<hdr->NS; k++) {
-		if (0)
-		; 
-		else if (hdr->TYPE==SCP_ECG) {
-/*			hdr->CHANNEL[k].Cal = Cal;
-			hdr->CHANNEL[k].Off = 0.0;
-*/		}
-		else {
-			hdr->CHANNEL[k].Cal = (hdr->CHANNEL[k].PhysMax-hdr->CHANNEL[k].PhysMin)/(hdr->CHANNEL[k].DigMax-hdr->CHANNEL[k].DigMin);
-			hdr->CHANNEL[k].Off =  hdr->CHANNEL[k].PhysMin-hdr->CHANNEL[k].Cal * hdr->CHANNEL[k].DigMin;
 		}
 	}
-}
-    fprintf(stdout,"File %s : -sopen-write\n", hdr->FileName);
 
-    hdr = sopen(dest, "w", hdr);
-    fprintf(stdout,"File %s : sopen-write\n", hdr->FileName);
-    {	
-	// write data 
-	// write(hdr->AS.rawdata, 4 ,hdr->NRec*hdr->SPR*hdr->NS, hdr->FILE.FID);
+	/* write file */
+	hdr = sopen(dest, "w", hdr);
 	swrite(hdr->data.block, hdr->NRec, hdr);
-    }
-    sclose(hdr);
-    free(hdr);
+    	sclose(hdr);
+    	free(hdr);
 }
