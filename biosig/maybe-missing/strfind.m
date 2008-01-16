478e3a72a3dc3def46dec1cac79854edd98b98c3
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2008-01-16 20:47:32 +0000

    support for FreeMat3.5 added

--- biosig/maybe-missing/strfind.m
+++ biosig/maybe-missing/strfind.m
@@ -11,68 +11,51 @@
 %% GNU General Public License for more details.
 %%
 %% You should have received a copy of the GNU General Public License
-%% along with this program; if not, write to the Free Software
-%% Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+%% along with Octave; see the file COPYING.  If not, write to the Free
+%% Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+%% 02110-1301, USA.
 
 %% -*- texinfo -*-
-%% @deftypefn {Function File} {@var{index} =} strfind (@var{str}, @var{pattern})
-%% @deftypefnx {Function File} {@var{index} =} strfind (@var{cellstr}, @var{pattern})
+%% @deftypefn {Function File} {@var{idx} =} strfind (@var{str}, @var{pattern})
+%% @deftypefnx {Function File} {@var{idx} =} strfind (@var{cellstr}, @var{pattern})
 %% Search for @var{pattern} in the string @var{str} and return the
-%% starting index of every such occurrence in the vector @var{index}.
+%% starting index of every such occurrence in the vector @var{idx}.
 %% If there is no such occurrence, or if @var{pattern} is longer
-%% than @var{str}, then @var{index} is the empty array @code{[]}.
+%% than @var{str}, then @var{idx} is the empty array @code{[]}.
 %%
 %% If the cell array of strings @var{cellstr} is specified instead of the
-%% string @var{str}, then @var{index} is a cell array of vectors, as specified
+%% string @var{str}, then @var{idx} is a cell array of vectors, as specified
 %% above.
-%% @end deftypefn
-%%
 %% @seealso{findstr, strmatch, strcmp, strncmp, strcmpi, strncmpi}
+%% @end deftypefn
 
-%% Author: a.schloegl <a dot schloegl at ieee dot org>
+%% Author: alois schloegl <a.schloegl@ieee.org>
 %% Created: 1 November 2004
-%% Adapted-By: wpyh
+%% Adapted-By: William Poetra Yoga Hadisoeseno <williampoetra@gmail.com>
 
 function idx = strfind (text, pattern)
 
-  lp  = length (pattern);
+	if (nargin ~= 2)
+		usage ('idx = strfind (text, pattern)');
+  	elseif (~ischar (pattern))
+    		error ('strfind: pattern must be a string value');
+  	end
 
-  if (nargin ~= 2)
-    usage ('idx = strfind (text, pattern)');
-  elseif (~ischar (pattern))
-    error ('strfind: pattern must be a string value');
-  end
+  	lp = length (pattern);
+  	if (ischar (text))
+    		idx = 1:(length (text) - lp + 1);
+  		k = 0;
+  		while ((k < lp) && ~isempty (idx))
+    	    		idx = idx(text(idx + k) == pattern(k+1));
+    	    		k = k+1;
+  		end
+  	elseif (iscellstr (text))
+    		idx = cell (size (text));
+    		for i = 1:(numel (text))
+			idx{i} = strfind(text{i}, pattern);
+    		end
+  	else
+    		error ('strfind: text must be a string or cell array of strings');
+  	end
 
-  if (ischar (text))
-    idx = 1:(length (text) - lp + 1);
-    k = 0;
-    while ((k < lp) && ~isempty (idx))
-        idx = idx(text(idx + k) == pattern(++k));
-    end
-
-  elseif (iscellstr (text))
-    idx = cell (size(text));
-    for i = 1:length(text)
-   	idx{i} = strfind(text{i},pattern);
-    end 	
-
-  else
-    error ('strfind: text must be a string or cell array of strings');
-  end
-
-
-% tests taken from examples in Matlab's documentation
-%!shared S, cstr
-%! S = "Find the starting indices of the pattern string";
-%! cstr = {"How much wood would a woodchuck chuck";
-%!         "if a woodchuck could chuck wood?"};
-%!assert(strfind(S,"in"),[2,15,19,45]);
-%!assert(isempty(strfind(S,"In")));
-%!assert(strfind(S," "),[5,9,18,26,29,33,41]);
-%!assert(strfind(cstr,"wood"),{[10,23];[6,28]});
-% demo, also from examples in Matlab's documentation
-%!demo
-%! cstr = {"How much wood would a woodchuck chuck";
-%!         "if a woodchuck could chuck wood?"};
-%! idx = strfind (cstr, "wood");
-%! idx{:,:}
+%% end
