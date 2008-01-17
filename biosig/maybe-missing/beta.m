4ba45e4b02eb5abd2ec1ad0ff7bcfadc30a7b60e
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2008-01-17 15:55:33 +0000

    Freemat requires standard m-syntax (not octave syntax)

--- biosig/maybe-missing/beta.m
+++ biosig/maybe-missing/beta.m
@@ -1,50 +1,49 @@
-## Copyright (C) 1996, 1997 John W. Eaton
-##
-## This file is part of Octave.
-##
-## Octave is free software; you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2, or (at your option)
-## any later version.
-##
-## Octave is distributed in the hope that it will be useful, but
-## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-## General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with Octave; see the file COPYING.  If not, write to the Free
-## Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-## 02110-1301, USA.
+%% Copyright (C) 2008 Alois Schloegl 
+%%
+%% This is free software; you can redistribute it and/or modify it
+%% under the terms of the GNU General Public License as published by
+%% the Free Software Foundation; either version 2, or (at your option)
+%% any later version.
+%%
+%% Octave is distributed in the hope that it will be useful, but
+%% WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%% General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with Octave; see the file COPYING.  If not, write to the Free
+%% Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+%% 02110-1301, USA.
 
-## -*- texinfo -*-
-## @deftypefn {Mapping Function} {} beta (@var{a}, @var{b})
-## Return the Beta function,
-## @iftex
-## @tex
-## $$
-##  B (a, b) = {\Gamma (a) \Gamma (b) \over \Gamma (a + b)}.
-## $$
-## @end tex
-## @end iftex
-## @ifinfo
-##
-## @example
-## beta (a, b) = gamma (a) * gamma (b) / gamma (a + b).
-## @end example
-## @end ifinfo
-## @end deftypefn
+%% -*- texinfo -*-
+%% @deftypefn {Mapping Function} {} beta (@var{a}, @var{b})
+%% Return the Beta function,
+%% @iftex
+%% @tex
+%% $$
+%%  B (a, b) = {\Gamma (a) \Gamma (b) \over \Gamma (a + b)}.
+%% $$
+%% @end tex
+%% @end iftex
+%% @ifinfo
+%%
+%% @example
+%% beta (a, b) = gamma (a) * gamma (b) / gamma (a + b).
+%% @end example
+%% @end ifinfo
+%% @end deftypefn
 
-## Author: KH <Kurt.Hornik@wu-wien.ac.at>
-## Created: 13 June 1993
-## Adapted-By: jwe
+%% Author: KH <Kurt.Hornik@wu-wien.ac.at>
+%% Created: 13 June 1993
+%% Adapted-By: jwe
+%% Adapted for FreeMat by : AS <a.schloegl@ieee.org> 
 
 function retval = beta (a, b)
 
-  if (nargin != 2)
-    usage ("beta (a, b)");
-  endif
+  if (nargin ~= 2)
+    usage ('beta (a, b)');
+  end
 
   retval = exp (gammaln (a) + gammaln (b) - gammaln (a+b));
 
-endfunction
+end
