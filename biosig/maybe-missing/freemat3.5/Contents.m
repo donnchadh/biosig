907a6cb5a86663249763c74e54cc3c154dd90050
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2008-01-23 11:37:29 +0000

    add more functions missing in Freemat v3.5

--- biosig/maybe-missing/freemat3.5/Contents.m
+++ biosig/maybe-missing/freemat3.5/Contents.m
@@ -1,8 +1,18 @@
 % BIOSIG/MAYBE-MISSING/FREEMAT3.5  contains functions that 
-% are missing in FreeMat v3.5. These improve the compatibility with 
-% FreeMat
+% are missing in FreeMat v3.5. These improve the compatibility 
+% of BioSig with FreeMat
 
-%	$Id: Contents.m,v 1.1 2008-01-19 21:55:45 schloegl Exp $
-%	CopyLeft (c) 2008 by Alois Schloegl <a.schloegl@ieee.org>	
+%	$Id: Contents.m,v 1.2 2008-01-23 11:37:29 schloegl Exp $
+%	Copyright (C) 2008 by Alois Schloegl <a.schloegl@ieee.org>	
 %	This is part of the BIOSIG project http://biosig.sf.net/
+
+
+see the list of files in this directory for missing files. 
+
+Other known incompatibilities are: 
+- FSEEK: no return argument
+- FREAD: fails if the number of avaiable bytes is insufficient.
+- NDIMS: trailing singleton dimensions are not ignored.
+
+
 
