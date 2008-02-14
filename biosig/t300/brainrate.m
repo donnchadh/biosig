968ba69c28cf9072f674d619a60edeac8d32710c
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2008-02-14 12:14:44 +0000

    speed up performance; wrong memory allocation corrected; avoiding loops

--- biosig/t300/brainrate.m
+++ biosig/t300/brainrate.m
@@ -27,11 +27,11 @@ function [br,sef90,sef95,br2] = brainrate(s,Fs,UC,A)
 % [1] Nada Pop-Jordanova and Jordan Pop-Jordanov
 %     Spectrum-weighted EEG frequency ("Brainrate") as a quantitative 
 %     indicator of arousal 
-%     Contributions, Sec. Biol. Med. Sci., MASA, XXVI, 2, p. 35–42 (2005)
-%     ISSN 0351–3254, UDK: 616.831-073.97
+%     Contributions, Sec. Biol. Med. Sci., MASA, XXVI, 2, p. 35-42 (2005)
+%     ISSN 0351-3254, UDK: 616.831-073.97
 
-%	$Id: brainrate.m,v 1.8 2007-07-26 07:36:56 schloegl Exp $
-%	Copyright (C) 2006,2007 by Alois Schloegl <a.schloegl@ieee.org>
+%	$Id: brainrate.m,v 1.9 2008-02-14 12:14:44 schloegl Exp $
+%	Copyright (C) 2006,2007,2008 by Alois Schloegl <a.schloegl@ieee.org>
 %	This is part of the BIOSIG-toolbox http://biosig.sf.net/
 
 % This library is free software; you can redistribute it and/or
@@ -52,21 +52,20 @@ function [br,sef90,sef95,br2] = brainrate(s,Fs,UC,A)
 
 [N,K] = size(s); 	% number of electrodes K, number of samples N
 MOP = 15; 		% order of autoregressive model 
-
 if nargin<3, 
 	UC = 0; 
 	B = []; 
 elseif nargin==3
 	if UC==0,
-		br = size(1,K); 
-		sef90 = size(1,K); 
-		sef95 = size(1,K); 
+		br = zeros(1,K); 
+		sef90 = zeros(1,K); 
+		sef95 = zeros(1,K); 
 	elseif (UC>0) & (UC<1),
 		B = UC; 
 		A = [1, UC-1];
-		br = size(N,K); 
-		sef90 = size(N,K); 
-		sef95 = size(N,K);
+		br = zeros(N,K); 
+		sef90 = zeros(N,K); 
+		sef95 = zeros(N,K);
 	else
 		fprintf(2,'ERROR BrainRate: update coefficient UC(%f) is outside of interval ]0,1[\n',UC); 
 		br =[]; sef90=[]; sef95=[];
@@ -86,21 +85,16 @@ for k2 = 1:K,
 	end;
 	
 	fi = [2:4:18];	% f_i list of center frequencies
-	for k1 = 1:size(a,1),
-		%h = freqz(sqrt(pe(k1)/Fs),[1,-a(k1,:)],f,Fs); 
-		% scaling not important because relative spectral distribution is used
-		h = freqz(1,[1,-a(k1,:)],fi,Fs); 
-		h2= abs(h); 
-		br(k1,k2) = sum(fi.*h2)/sum(h2); 
-	end; 
+	h2 = 1./abs(1-a*exp(([1:MOP]'*fi)*(-i*2*pi)/Fs));
+	br(:,k2) = (h2*fi')./sum(h2,2); 
 
 	if nargout>1,
+		f = (0:256)/512; 
+		h = 1./(1-a*exp(([1:MOP]'*f)*(-i*2*pi)/Fs));
+		h2= cumsum(real(h.*conj(h)),2);
 		for k1 = 1:size(a,1),
-			f = (0:256)/512*Fs; 
-			h = freqz(1,[1,-a(k1,:)],f,Fs); 
-			h2= cumsum(real(h.*conj(h)),2);
-			sef90(k1,k2) = f(min(find(h2>=.90*h2(end)))); 
-			sef95(k1,k2) = f(min(find(h2>=.95*h2(end)))); 
+			sef90(k1,k2) = f(min(find(h2(k1,:)>=.90*h2(k1,end)))); 
+			sef95(k1,k2) = f(min(find(h2(k1,:)>=.95*h2(k1,end)))); 
 		end; 
 	end; 
 end;
