d0241f01e9801c960557fc4c1c4749e44cf37397
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2008-03-14 22:08:40 +0000

    return of HDR struct successfully implemented

--- biosig4c++/mexSLOAD.cpp
+++ biosig4c++/mexSLOAD.cpp
@@ -1,22 +1,15 @@
 /*
 
-    $Id: mexSLOAD.cpp,v 1.9 2008-03-05 22:29:26 schloegl Exp $
-    Copyright (C) 2007 Alois Schloegl <a.schloegl@ieee.org>
+    $Id: mexSLOAD.cpp,v 1.10 2008-03-14 22:08:40 schloegl Exp $
+    Copyright (C) 2007,2008 Alois Schloegl <a.schloegl@ieee.org>
     This file is part of the "BioSig for C/C++" repository 
     (biosig4c++) at http://biosig.sf.net/ 
 
-    BioSig is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 3
+    of the License, or (at your option) any later version.
 
-    BioSig is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with BioSig. If not, see <http://www.gnu.org/licenses/>.
 
  */
 
@@ -70,52 +63,6 @@ void mexFunction(
 			if (VERBOSE_LEVEL>8) fprintf(stdout,"[112] SOPEN-R finished\n");
 
 
-#ifdef ONLY_IF_ARGOUT2_IMPLEMENTED
-			/* this part needs fixing */			
-			
-			hdr2ascii(hdr,stdout,4);	
-			if (nlhs>1) {
-				char* mexFileName = (char*)mxMalloc(strlen(hdr->FileName)+1); 
-				mxArray *mexNS    = mxCreateNumericMatrix(2,1,mxUINT16_CLASS,mxREAL);
-				mxArray *mexSPR   = mxCreateNumericMatrix(2,1,mxUINT32_CLASS,mxREAL);
-				mxArray *mexNRec  = mxCreateNumericMatrix(2,1,mxUINT64_CLASS,mxREAL);
-
-if (VERBOSE_LEVEL>8) mexPrintf("[200]\n");
-				uint16_t numfields;
-				mxArray *HDR, *tmp;
-				void* ptr;
-				const char *fnames[] = {"TYPE","NS","SPR","NRec","SampleRate","FileName","FILE","Patient",NULL};
-				for (numfields=0; fnames[numfields++] != 0; );
-				plhs[1] = mxCreateStructMatrix(1, 1, --numfields, fnames);
-				HDR = plhs[1];
-
-mexPrintf("[221] %x %x %i\n",tmp,mxGetData(tmp),4);
-//				memcpy(mxGetData(mexNS),&(hdr->NS),2);
-mexPrintf("[221] %x\n",HDR);
-				mxSetFieldByNumber(HDR,1,2,mexNS);
-
-				memcpy(mxGetData(mexSPR),&(hdr->SPR),4);
-//				mxSetFieldByNumber(HDR,1,3,mexSPR);
-
-				memcpy(mxGetData(mexNRec),&(hdr->NRec),8);
-//				mxSetFieldByNumber(HDR,1,4,mexNRec);
-
-//
-if (VERBOSE_LEVEL>8) mexPrintf("[205]\n");
-/*
-				tmp = mxCreateNumericMatrix(1,1,mxUINT32_CLASS,mxREAL);
-				//memcpy(mxGetData(tmp),hdr->SPR);
-				mxSetField(HDR,1,"SPR",tmp);
-				tmp = mxCreateNumericMatrix(1,1,mxUINT64_CLASS,mxREAL);
-				//*(uint64_t*)mxGetData(tmp) = hdr->NRec;
-				mxSetField(HDR,1,"NRec",tmp);
-*/
-			}	
-
-#endif
-			plhs[0] = mxCreateDoubleMatrix(hdr->SPR * hdr->NRec, hdr->NS, mxREAL);
-//			hdr->data.block = mxGetPr(plhs[0]);
-
 			if (VERBOSE_LEVEL>8) fprintf(stdout,"[113] SOPEN-R finished\n");
 
 			hdr->FLAG.OVERFLOWDETECTION = (hdr->TYPE!=MFER);
@@ -123,11 +70,76 @@ if (VERBOSE_LEVEL>8) mexPrintf("[205]\n");
 	
 			if (VERBOSE_LEVEL>8) fprintf(stdout,"[121]\n");
 
-			count = sread(mxGetPr(plhs[0]), 0, hdr->NRec, hdr);
+			count = sread(NULL, 0, hdr->NRec, hdr);
 			if ((status=serror())) return;  
 
 			if (VERBOSE_LEVEL>8) 
 				fprintf(stdout,"\n[129] SREAD on %s successful [%i,%i].\n",hdr->FileName,hdr->data.size[0],hdr->data.size[1]);
+
+			hdr->NRec = count; 
+			plhs[0] = mxCreateDoubleMatrix(hdr->SPR * count, hdr->NS, mxREAL);
+			memcpy((void*)mxGetPr(plhs[0]),(void*)hdr->data.block,hdr->SPR * count * hdr->NS * sizeof(biosig_data_type));
+	        	free(hdr->data.block);	
+
+
+//			hdr2ascii(hdr,stdout,4);	
+
+			if (nlhs>1) { 
+				char* mexFileName = (char*)mxMalloc(strlen(hdr->FileName)+1); 
+
+				mxArray *HDR, *tmp, *Patient, *EVENT;
+				uint16_t numfields;
+				const char *fnames[] = {"TYPE","NS","SPR","NRec","SampleRate","FileName","FILE","Patient","EVENT",NULL};
+				for (numfields=0; fnames[numfields++] != 0; );
+				HDR = mxCreateStructMatrix(1, 1, --numfields, fnames);
+
+				mxSetField(HDR,0,"NS",mxCreateDoubleScalar(hdr->NS));
+				mxSetField(HDR,0,"SPR",mxCreateDoubleScalar(hdr->SPR));
+				mxSetField(HDR,0,"NRec",mxCreateDoubleScalar(hdr->NRec));
+				mxSetField(HDR,0,"SampleRate",mxCreateDoubleScalar(hdr->SampleRate));
+
+				mxSetField(HDR,0,"FileName",mxCreateCharMatrixFromStrings(1,&hdr->FileName));
+
+
+				const char *event_fields[] = {"N","SampleRate","TYP","POS","DUR","CHN",NULL};
+				for (numfields=0; event_fields[numfields++] != 0; );
+				EVENT = mxCreateStructMatrix(1, 1, --numfields, event_fields);
+				mxSetField(EVENT,0,"N",mxCreateDoubleScalar(hdr->EVENT.N));
+				mxSetField(EVENT,0,"SampleRate",mxCreateDoubleScalar(hdr->EVENT.SampleRate));
+
+				mxArray *TYP = mxCreateDoubleMatrix(hdr->EVENT.N,1, mxREAL);
+				mxArray *POS = mxCreateDoubleMatrix(hdr->EVENT.N,1, mxREAL);
+				for (size_t k=0; k<hdr->EVENT.N; ++k) {
+					*(mxGetPr(TYP)+k) = (double)hdr->EVENT.TYP[k];
+					*(mxGetPr(POS)+k) = (double)hdr->EVENT.POS[k];
+				} 
+				mxSetField(EVENT,0,"TYP",TYP);
+				mxSetField(EVENT,0,"POS",POS);
+				if (hdr->EVENT.DUR != NULL) {
+					mxArray *DUR = mxCreateDoubleMatrix(hdr->EVENT.N,1, mxREAL);
+					mxArray *CHN = mxCreateDoubleMatrix(hdr->EVENT.N,1, mxREAL);
+					for (size_t k=0; k<hdr->EVENT.N; ++k) {
+						*(mxGetPr(DUR)+k) = (double)hdr->EVENT.DUR[k];
+						*(mxGetPr(CHN)+k) = (double)hdr->EVENT.CHN[k];
+					} 
+					mxSetField(EVENT,0,"DUR",DUR);
+					mxSetField(EVENT,0,"CHN",CHN);
+				}
+				mxSetField(HDR,0,"EVENT",EVENT);
+
+				const char *patient_fields[] = {"Sex","Handedness","Id","Name","Weight","Height",NULL};
+				for (numfields=0; patient_fields[numfields++] != 0; );
+				Patient = mxCreateStructMatrix(1, 1, --numfields, patient_fields);
+//				mxSetField(Patient,0,"Name",mxCreateCharMatrixFromStrings(1,(const char**)&(hdr->Patient.Name)));
+//				mxSetField(Patient,0,"Id",mxCreateCharMatrixFromStrings(1,(const char**)&(hdr->Patient.Id)));
+				mxSetField(Patient,0,"Handedness",mxCreateDoubleScalar(hdr->Patient.Handedness));
+				mxSetField(Patient,0,"Sex",mxCreateDoubleScalar(hdr->Patient.Sex));
+				mxSetField(Patient,0,"Weight",mxCreateDoubleScalar(hdr->Patient.Weight));
+				mxSetField(Patient,0,"Height",mxCreateDoubleScalar(hdr->Patient.Height));
+
+				mxSetField(HDR,0,"Patient",Patient);
+				plhs[1] = HDR;
+			}	
 
 			if (VERBOSE_LEVEL>8) fprintf(stdout,"[131] going for SCLOSE\n");
 			sclose(hdr);
