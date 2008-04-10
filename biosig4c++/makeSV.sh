57f10815f63833a868a4401bdbec6e26b631f4cc
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2008-04-10 06:51:11 +0000

    include latest table of eventcodes

--- biosig4c++/makeSV.sh
+++ biosig4c++/makeSV.sh
@@ -5,11 +5,16 @@
 qmake -o QMakefile
 make -f QMakefile
 
+# symbolic link to latest biosig4c
 rm -rf ../sigviewer/extern/biosig.h
 ln -s ../../biosig4c++/biosig.h ../sigviewer/extern/biosig.h 
 rm ../sigviewer/extern/libbiosig.a
 ln -s ../../biosig4c++/libbiosig.a ../sigviewer/extern/libbiosig.a
 
+# copy latest table of event codes 
+cp ../biosig/doc/eventcodes.txt ../sigviewer/bin/
+
+# build sigviewer
 cd ../sigviewer/src/
 qmake
 make
