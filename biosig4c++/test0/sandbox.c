b64b3266645a0abd6ea00dd89de6ff35be1b20d2
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2008-08-14 10:52:33 +0000

    move experimental functions into sandbox

---
+++ biosig4c++/test0/sandbox.c
@@ -0,0 +1,39 @@
+/*
+
+    sandbox is used for development and under constraction work
+    The functions here are either under construction or experimental. 
+    The functions will be either fixed, then they are moved to another place;
+    or the functions are discarded. Do not rely on the interface in this function
+       	
+
+    $Id: sandbox.c,v 1.1 2008-08-14 10:52:32 schloegl Exp $
+    Copyright (C) 2008 Alois Schloegl <a.schloegl@ieee.org>
+    This file is part of the "BioSig for C/C++" repository 
+    (biosig4c++) at http://biosig.sf.net/ 
+
+    BioSig is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 3
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+
+
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#include "../biosig-dev.h"
+
+extern int VERBOSE_LEVEL;
+
+
+
