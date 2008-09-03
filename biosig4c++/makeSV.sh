151f4fea5e403726013ff24692f11e71818e1562
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2008-09-03 08:34:26 +0000

    improve Makefile - mostly for Win32/MinGW

--- biosig4c++/makeSV.sh
+++ biosig4c++/makeSV.sh
@@ -1,3 +1,12 @@
+####### Batch to Make SigViewer from biosig4c++ ######################
+###
+###  $Id: makeSV.sh,v 1.5 2008-09-03 08:34:26 schloegl Exp $
+###  Copyright (C) 2008 Alois Schloegl <a.schloegl@ieee.org>
+###  This file is part of the "BioSig for C/C++" repository 
+###  (biosig4c++) at http://biosig.sf.net/ 
+###
+##############################################################
+
 # make SigViewer from biosig4c++
 # rm -rf ../sigviewer/extern
 # ln -s ../biosig4c++ ../sigviewer/extern 
@@ -5,15 +14,9 @@
 qmake -o QMakefile
 make -f QMakefile
 
-# symbolic link to latest biosig4c
-rm -rf ../sigviewer/extern/biosig.h
-ln -s ../../biosig4c++/biosig.h ../sigviewer/extern/biosig.h 
-rm ../sigviewer/extern/libbiosig.a
-ln -s ../../biosig4c++/libbiosig.a ../sigviewer/extern/libbiosig.a
-
 # copy latest table of event codes 
 cp ../biosig/doc/eventcodes.txt ../sigviewer/bin/
-cp ../biosig/doc/units.csv      ../sigviewer/bin/
+#cp ../biosig/doc/units.csv      ../sigviewer/bin/
 
 # build sigviewer
 cd ../sigviewer/src/
