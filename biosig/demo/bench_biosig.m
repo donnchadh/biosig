f59319744851abe60c77df7d10b71b29a9b79357
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2009-04-21 10:39:20 +0000

    fix minor issues, make it read for a comparison, output results

--- biosig/demo/bench_biosig.m
+++ biosig/demo/bench_biosig.m
@@ -14,7 +14,7 @@
 %
 
 
-%	$Id: bench_biosig.m,v 1.6 2006-10-05 13:50:08 schloegl Exp $
+%	$Id: bench_biosig.m,v 1.7 2009-04-21 10:39:20 schloegl Exp $
 %	Copyright (C) 2005,2006 by Alois Schloegl <a.schloegl@ieee.org>
 %    	This is part of the BIOSIG-toolbox http://biosig.sf.net/
 
@@ -34,39 +34,42 @@
 % Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 % Boston, MA  02111-1307, USA.
 
-
+clear 
 
 if ~exist('l1.gdf','file') % download test file if not available
         system('wget http://hci.tugraz.at/schloegl/bci/bci7/l1.gdf');
 end;
 
-tic; K=0;
+K=0; tic; t0=cputime();
 
 %[signal,HDR]=sload({[p,'x_train'],[p,'x_test']});
 [signal,HDR]=sload('l1.gdf');
 
-K=K+1;jo{K}='sload l1.gdf'; t(K)=toc
+K=K+1;jo{K}='sload l1.gdf'; t(K)=cputime()-t0,t1(K)=toc
 z = zscore(signal);
 
 try
         bp = bandpower(z,HDR.SampleRate);
         bp(bp<-10)=NaN;
-        K=K+1;jo{K}='bandpower'; t(K)=toc
+        K=K+1;jo{K}='bandpower'; t(K)=cputime()-t0,t1(K)=toc
 catch, end;
 
 [SIGMA1,PHI1,OMEGA1,m01,m11,m21] = wackermann(z(:,1:2),HDR.SampleRate);
 [SIGMA2,PHI2,OMEGA2,m02,m12,m22] = wackermann(z(:,2:3),HDR.SampleRate);
-K=K+1;jo{K}='wackermann'; t(K)=toc
+K=K+1;jo{K}='wackermann'; t(K)=cputime()-t0,t1(K)=toc
 
 [a,f,s] = barlow(z,HDR.SampleRate);
 BARLOW = [a,f,s];
-K=K+1;jo{K}='barlow'; t(K)=toc
+K=K+1;jo{K}='barlow'; t(K)=cputime()-t0,t1(K)=toc
 
 [A,M,C] = hjorth(z,HDR.SampleRate);
 HJORTH = [A,M,C];
-K=K+1;jo{K}='hjorth'; t(K)=toc
+K=K+1;jo{K}='hjorth'; t(K)=cputime()-t0,t1(K)=toc
 
 uc  = 30:5:80;
+[F] = tdp(z,3,2^(-uc(7)/8));
+K=K+1;jo{K}='TDP'; t(K)=cputime()-t0,t1(K)=toc
+
 a = [];
 for ch = 1:size(signal,2),
         fprintf(1,'.%i',ch);
@@ -77,62 +80,54 @@ for ch = 1:size(signal,2),
         X = tvaar(signal(:,ch),X);
 
         a = [a,X.AAR];
-        K = K+1; jo{K} = ['aar #',int2str(ch)]; t(K)=toc
+        K = K+1; jo{K} = ['aar #',int2str(ch)]; t(K)=cputime()-t0,t1(K)=toc
 end
-
-uc  = 30:5:80;
-ab = [];
-q = 8; 
-for ch = 1:size(signal,2),
-        fprintf(1,'.%i',ch);
-        INI.MOP = [0,3,0];
-        INI.UC  = 2^(-uc(7)/8);
-
-        s = center(filter(ones(1,q),q,abs(diff(signal(:,ch)))));
-        X = tvaar(s,INI);
-        X = tvaar(s,X);
-
-        ab = [ab,X.AAR];
-        K = K+1; jo{K} = ['aar #',int2str(ch)]; t(K)=toc
-end
-
 
 %% K-fold Crossvalidation
-K = 5;
-ng = ceil([0:length(HDR.Classlabel)-1]'/length(HDR.Classlabel)*K);
+KK = 5;
+ng = ceil([0:length(HDR.Classlabel)-1]'/length(HDR.Classlabel)*KK);
 Classifiers = {'LD2','LD3','LD4','MDA','MD2','MD3','GRB','GRB2','QDA','MQU','INQ','Cauchy','SVM','SVM11','RBF','REG'};
-Classifiers = {'LDA','LDA/GSVD','LD2','LD3','LD4','REG','MDA','MD2','MD3','GRB','GRB2','QDA','MQU','IMQ','Cauchy','LDA/sparse','SVM:LIB','SVM:OSU'};
+Classifiers = {'LDA','LDA/GSVD','LD2','LD3','LD4','NBC','aNBC','REG','MDA','MD2','MD3','GRB','GRB2','QDA','MQU','IMQ','Cauchy','LDA/sparse','SVM:LIB','SVM:OSU'};
 
 
 try,
         CC1 = findclassifier(bp,HDR.EVENT.POS(HDR.EVENT.TYP==hex2dec('300'))-1,[HDR.Classlabel,ng],reshape(1:1152,16,72)',[1:72]'>24,'LD3');
         CC1.TSD.T = CC1.TSD.T/HDR.SampleRate;
-        K=K+1; jo{K}='findclassifier bp'; t(K)=toc
+        K=K+1; jo{K}='findclassifier bp LD3'; t(K)=cputime()-t0,t1(K)=toc
 catch,
 end;
 
-
-
 CC2 = findclassifier(BARLOW,HDR.EVENT.POS(HDR.EVENT.TYP==hex2dec('300'))-1,[HDR.Classlabel,ng],reshape(1:1152,16,72)',[1:72]'>24,'LD3');
 CC2.TSD.T = CC2.TSD.T/HDR.SampleRate;
-K=K+1; jo{K}='findclassifier barlow LD3'; t(K)=toc
+K=K+1; jo{K}='findclassifier barlow LD3'; t(K)=cputime()-t0,t1(K)=toc
 CC3 = findclassifier(HJORTH,HDR.EVENT.POS(HDR.EVENT.TYP==hex2dec('300'))-1,[HDR.Classlabel,ng],reshape(1:1152,16,72)',[1:72]'>24,'LD3');
 CC3.TSD.T = CC3.TSD.T/HDR.SampleRate;
-K=K+1; jo{K}='findclassifier hjorth LD3'; t(K)=toc
+K=K+1; jo{K}='findclassifier hjorth LD3'; t(K)=cputime()-t0,t1(K)=toc
 CC4 = findclassifier(a,HDR.EVENT.POS(HDR.EVENT.TYP==hex2dec('300'))-1,[HDR.Classlabel,ng],reshape(1:1152,16,72)',[1:72]'>24,'LD3');
 CC4.TSD.T = CC4.TSD.T/HDR.SampleRate;
-K=K+1; jo{K}='findclassifier aar LD3'; t(K)=toc
+K=K+1; jo{K}='findclassifier aar LD3'; t(K)=cputime()-t0,t1(K)=toc
 CC5 = findclassifier([SIGMA1,PHI1,OMEGA1,SIGMA2,PHI2,OMEGA2],HDR.EVENT.POS(HDR.EVENT.TYP==hex2dec('300'))-1,[HDR.Classlabel,ng],reshape(1:1152,16,72)',[1:72]'>24,'LD3');
 CC5.TSD.T = CC5.TSD.T/HDR.SampleRate;
-K=K+1; jo{K}='findclassifier Wackermann LD3'; t(K)=toc
+K=K+1; jo{K}='findclassifier Wackermann LD3'; t(K)=cputime()-t0,t1(K)=toc
+CC6 = findclassifier([SIGMA1,PHI1,OMEGA1,SIGMA2,PHI2,OMEGA2],HDR.EVENT.POS(HDR.EVENT.TYP==hex2dec('300'))-1,[HDR.Classlabel,ng],reshape(1:1152,16,72)',[1:72]'>24,'LD3');
+CC6.TSD.T = CC6.TSD.T/HDR.SampleRate;
+K=K+1; jo{K}='findclassifier TDP LD3'; t(K)=cputime()-t0,t1(K)=toc
+
+fprintf(1,'Version: %s\ncputime\t toc[s]\ttask\n================================\n',version); 
+t0 = [0,0]; 
+for k=1:K,
+	fprintf(1,'%7.3f\t%7.3f\t%s\n',t(k)-t0(1),t1(k)-t0(2),jo{k});
+	t0 = [t(k),t1(k)];
+end; 
+return; 
 
 clear ACC KAP MI AUC
 TI = 36; 35;27;
-for k=16:17,18:length(Classifiers); 
+for k=[1:18], %:17,18:length(Classifiers); 
         try
         CC = findclassifier(a,HDR.EVENT.POS(HDR.EVENT.TYP==hex2dec('300'))-1,[HDR.Classlabel,ng],(1:16:1152)',[1:72]'==TI,Classifiers{k});
         CC.TSD.T = CC.TSD.T/HDR.SampleRate;
-        K=K+1; jo{K}=['findclassifier AAR ',Classifier{k}]; t(K)=toc
+        K=K+1; jo{K}=['findclassifier AAR ',Classifier{k}]; t(K)=cputime()-t0,t1(K)=toc
         CC90{k}=CC.TSD; 
         ACC(:,k)=CC90{k}.ACC00; 
         KAP(:,k)=CC90{k}.KAP00; 
@@ -151,6 +146,7 @@ plot(CC90{k}.T,MI);
 subplot(224),
 plot(CC90{k}.T,AUC);
 legend(Classifiers);
+
 
 figure(2)
 subplot(221),
@@ -191,7 +187,7 @@ catch
 end;
 fid = fopen(outfile,'a');
 fprintf(fid,'\n\nDate:\t%s\n',date);
-fprintf(fid,'Revision:\t$Id: bench_biosig.m,v 1.6 2006-10-05 13:50:08 schloegl Exp $\n');
+fprintf(fid,'Revision:\t$Id: bench_biosig.m,v 1.7 2009-04-21 10:39:20 schloegl Exp $\n');
 fprintf(fid,'Computer:\t%s\nSoftware:\t%s\nVersion:\t%s\n',computer,om,version);
 
 tmp = [diff([0,t(:)']);t(:)']';
