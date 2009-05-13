db3b30e213768da5e5e83c42fbbdf6e3a50f5e9e
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2009-05-13 09:14:11 +0000

    weigthing of data supported; improve interface to plota

--- biosig/t400/xval.m
+++ biosig/t400/xval.m
@@ -4,13 +4,17 @@ function [R,CC]=xval(D,classlabel,MODE,arg4)
 %  [R,CC] = xval(D,classlabel)
 %  .. = xval(D,classlabel,CLASSIFIER)
 %  .. = xval(D,classlabel,CLASSIFIER,type)
-%  .. = xval(D,[classlabel,NG],CLASSIFIER)
-%  .. = xval(D,[classlabel,NG,W],CLASSIFIER)
+%  .. = xval(D,[classlabel,W],CLASSIFIER)
+%  .. = xval(D,[classlabel,W,NG],CLASSIFIER)
 %
 % Input:
-%    D 	data features (one feature per column, one sample per row)
+%    D:	data features (one feature per column, one sample per row)
 %    classlabel  classlabel (same number of rows)
 %    CLASSIFIER can be any classifier supported by train_sc (default='LDA')
+%	{'MDA','MD2','LD2','LD3','LD4','LD5','LD6','NBC','aNBC','WienerHopf','REG','LDA/GSVD','MDA/GSVD', 'LDA/sparse','MDA/sparse','RDA','GDBC','SVM','RBF'}
+%    W:	weights for each sample (row) in D. 
+%	default: W=ones(:,1),
+%	number of elements in W must match the number of rows of D 
 %    NG: used to define the type of cross-valdiation
 % 	Leave-One-Out-Method (LOOM): NG = [1:length(classlabel)]' (default)
 % 	Leave-K-Out-Method: NG = ceil([1:length(classlabel)]'/K)
@@ -18,28 +22,33 @@ function [R,CC]=xval(D,classlabel,MODE,arg4)
 %	group-wise XV (if samples are not indepentent) can be also defined here
 %	samples from the same group (dependent samples) get the same identifier
 %	samples from different groups get different classifiers
-%    W  weight of each sample [default: ones(:,1)]
-%    TYPE  defines the type of cross-validation procedure if NG is not specified 
+%    TYPE:  defines the type of cross-validation procedure if NG is not specified 
 %	'LOOM'  leave-one-out-method
 %       k	k-fold crossvalidation
 %
 % OUTPUT: 
 %    R contains the resulting performance metric
-%    CC contains the parameters of the classifier  
+%    CC contains the classifier  
 %
-% see also: TRAIN_SC, TEST_SC, CLASSIFY
+%    plota(R) shows the confusion matrix of the results
+%
+% see also: TRAIN_SC, TEST_SC, CLASSIFY, PLOTA
 %
 % References: 
 % [1] R. Duda, P. Hart, and D. Stork, Pattern Classification, second ed. 
 %       John Wiley & Sons, 2001. 
+% [2] A. Schlögl, J. Kronegg, J.E. Huggins, S. G. Mason;
+%       Evaluation criteria in BCI research.
+%       (Eds.) G. Dornhege, J.R. Millan, T. Hinterberger, D.J. McFarland, K.-R.Müller;
+%       Towards Brain-Computer Interfacing, MIT Press, 2007, p.327-342
 
-%	$Id: xval.m,v 1.1 2008/12/04 15:34:44 schloegl Exp $
-%	Copyright (C) 2008 by Alois Schloegl <a.schloegl@ieee.org>	
+%	$Id$
+%	Copyright (C) 2008,2009 by Alois Schloegl <a.schloegl@ieee.org>	
 %    	This is part of the BIOSIG-toolbox http://biosig.sf.net/
 
 % This program is free software; you can redistribute it and/or
 % modify it under the terms of the GNU General Public License
-% as published by the Free Software Foundation; either version 2
+% as published by the Free Software Foundation; either version 3
 % of the  License, or (at your option) any later version.
 % 
 % This program is distributed in the hope that it will be useful,
@@ -67,16 +76,21 @@ if sz(1)~=size(classlabel,1),
         error('length of data and classlabel does not fit');
 end;
 
+NG = [1:sz(1)]';	
 W = ones(sz(1),1);
+% use only valid samples
+ix0 = find(~any(isnan([classlabel]),2));
 if size(classlabel,2)>1,
 	%% group-wise classvalidation
-	[tmp,tmp1,NG] = unique(classlabel(:,2));
+	W = classlabel(:,2);
 	if size(classlabel,2)>2,
-		W = classlabel(:,3);
-	end; 
+		[Label,tmp1,NG] = unique(classlabel(:,3));
+	end;
+
 elseif nargin<4
 	%% LOOM 
 	NG = [1:sz(1)]';	
+
 elseif isnumeric(arg4)
 	if isscalar(arg4)  
 	% K-fold XV
@@ -89,17 +103,6 @@ elseif strcmpi(arg4,'LOOM')
 	NG = [1:sz(1)]';	
 end; 
 
-%CC.Labels = unique(classlabel);
-CC.Labels = 1:max(classlabel);
-
-% remove all NaN's
-ix = any(isnan([D,classlabel]),2);
-D(ix,:)=[];
-classlabel(ix,:)=[];
-W(ix,:)=[];
-sz = size(D);
-
-
 sz = size(D);
 if sz(1)~=length(classlabel),
         error('length of data and classlabel does not fit');
@@ -108,18 +111,30 @@ if ~isfield(MODE,'hyperparameter')
         MODE.hyperparameter = [];
 end
 
-
-for k = 1:sz(1),
- 	ix = find(NG~=k);
+cl = repmat(NaN,size(classlabel,1),1);
+for k = 1:max(NG),
+ 	ix = ix0(NG(ix0)~=k);
 	CC = train_sc(D(ix,:),classlabel(ix,1),MODE,W(ix));
- 	ix = find(NG==k);
+ 	ix = ix0(NG(ix0)==k);
 	r  = test_sc(CC,D(ix,:));
 	cl(ix,1) = r.classlabel;
 end; 
-R = kappa(cl,classlabel(:,1),[],W);
+
+R = kappa(classlabel(:,1),cl,'notIgnoreNAN',W);
 R.ERR = 1-R.ACC; 
+if isnumeric(R.Label)
+	R.Label = cellstr(int2str(R.Label)); 
+end; 		
+R.datatype='confusion';
+R.data = R.H; 
 
 if nargout>1,
 	% final classifier 
-	CC = train_sc(D,classlabel,MODE);
+	if all(W==1)
+		CC = train_sc(D,classlabel,MODE);
+	else	
+		CC = train_sc(D,classlabel,MODE,W);
+	end; 	
+	CC.Labels = 1:max(classlabel);
+	%CC.Labels = unique(classlabel);
 end; 
