c2c11f036e41bdc1a01067d61cc9a9136c80b1eb
schloegl <schloegl@522a17e7-984b-0410-9de2-b16198d6e48b>
2009-06-05 12:10:39 +0000

    fix for files with less than two channels

--- biosig4c++/example.py
+++ biosig4c++/example.py
@@ -7,12 +7,14 @@ def load(fname):
     HDR = biosig.constructHDR(0, 0)
     HDR = biosig.sopen(fname, "r", HDR);
 
-    for i in range(HDR.NS):
-        HDR.CHANNEL[i].OnOff = 0
+#	turn off all channels 
+#    for i in range(HDR.NS):
+#        HDR.CHANNEL[i].OnOff = 0
 
-    HDR.CHANNEL[0].OnOff = 1
-    HDR.CHANNEL[1].OnOff = 1
-    HDR.CHANNEL[HDR.NS-1].OnOff = 1
+#	turn on specific channels 
+#    HDR.CHANNEL[0].OnOff = 1
+#    HDR.CHANNEL[1].OnOff = 1
+#    HDR.CHANNEL[HDR.NS-1].OnOff = 1
 
     data = biosig.sread(0, HDR.NRec, HDR)
 
